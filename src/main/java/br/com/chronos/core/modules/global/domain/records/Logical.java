719a61199c55a8ec8e3b7426cb06d67e455f36f3
kaufon <kauanfvale@gmail.com>
2025-03-12 17:56:55 -0300

    🌐domain: add Logical record

---
+++ src/main/java/br/com/chronos/core/modules/global/domain/records/Logical.java
@@ -0,0 +1,15 @@
+package br.com.chronos.core.modules.global.domain.records;
+
+public record Logical(boolean value) {
+  public static Logical create(boolean value) {
+    return new Logical(value);
+  }
+
+  public boolean isTrue() {
+    return value;
+  }
+
+  public boolean isFalse() {
+    return !value;
+  }
+}
