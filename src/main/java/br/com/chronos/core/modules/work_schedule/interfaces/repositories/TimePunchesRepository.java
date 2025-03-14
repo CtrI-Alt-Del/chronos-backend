5ba1556472eebc61f00a7a30f66538365a24055d
JohnPetros <joaopedro.nc@outlook.com>
2025-03-14 06:34:29 -0300

    📑 interfaces: add TimePunchesRepository

---
+++ src/main/java/br/com/chronos/core/modules/work_schedule/interfaces/repositories/TimePunchesRepository.java
@@ -0,0 +1,12 @@
+package br.com.chronos.core.modules.work_schedule.interfaces.repositories;
+
+import java.util.Optional;
+
+import br.com.chronos.core.modules.global.domain.records.Id;
+import br.com.chronos.core.modules.work_schedule.domain.entities.TimePunch;
+
+public interface TimePunchesRepository {
+  Optional<TimePunch> findById(Id id);
+
+  void update(TimePunch timePunch);
+}
