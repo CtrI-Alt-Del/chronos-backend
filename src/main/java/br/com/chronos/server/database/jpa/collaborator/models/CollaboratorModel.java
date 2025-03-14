a77673987f715536a7b11eae39aa36494df89817
kaufon <kauanfvale@gmail.com>
2025-03-14 17:58:16 -0300

    💾database: create collaborator mapper

---
+++ src/main/java/br/com/chronos/server/database/jpa/collaborator/models/CollaboratorModel.java
@@ -0,0 +1,51 @@
+package br.com.chronos.server.database.jpa.collaborator.models;
+
+import java.util.UUID;
+
+import br.com.chronos.core.modules.collaboration.domain.records.CollaboratorRole.Role;
+import br.com.chronos.core.modules.collaboration.domain.records.CollaboratorSector.Sector;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Id;
+
+@Entity
+@Table(name = "collaborators")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class CollaboratorModel{
+    @Id
+    private UUID id;
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+    @Column(name = "email", nullable = false, unique = true, length = 100)
+    private String email;
+    @Column(name = "cpf", nullable = false, unique = true, length = 11)
+    private String cpf;
+    @Column(name = "password", nullable = false, length = 20)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "sector", nullable = false)
+    private Sector sector;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive;
+
+}
