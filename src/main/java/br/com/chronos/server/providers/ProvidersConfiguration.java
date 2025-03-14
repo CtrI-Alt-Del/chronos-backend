0eed76a276d4b87b76a3ee384e7e789ea76ad2c0
kaufon <kauanfvale@gmail.com>
2025-03-14 13:57:37 -0300

    🛜api: make dotenv provider

---
+++ src/main/java/br/com/chronos/server/providers/ProvidersConfiguration.java
@@ -0,0 +1,8 @@
+package br.com.chronos.server.providers;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProvidersConfiguration {
+
+}
---
+++ src/main/java/br/com/chronos/server/providers/env/DotenvProvider.java
@@ -0,0 +1,17 @@
+package br.com.chronos.server.providers.env;
+
+import br.com.chronos.core.modules.global.domain.interfaces.providers.EnvProvider;
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class DotenvProvider implements EnvProvider {
+  private final Dotenv dotenv;
+
+  public DotenvProvider() {
+    dotenv = Dotenv.load();
+  }
+
+  @Override
+  public String get(String key) {
+    return dotenv.get(key);
+  }
+}
