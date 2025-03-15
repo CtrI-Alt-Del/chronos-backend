ed8841b2e21d01919c0babdf0db3a5f0e67dc074
kaufon <kauanfvale@gmail.com>
2025-03-14 23:08:44 -0300

    🚧WIP: findMany method in collaborators repository

--- src/main/java/br/com/chronos/server/database/jpa/collaborator/repositories/JpaCollaboratorRepository.java
+++ src/main/java/br/com/chronos/server/database/jpa/collaborator/repositories/JpaCollaboratorRepository.java
@@ -4,13 +4,16 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.chronos.core.modules.collaboration.domain.entities.Collaborator;
 import br.com.chronos.core.modules.collaboration.interfaces.repositories.CollaboratorsRepository;
+import br.com.chronos.core.modules.global.domain.records.Collection;
 import br.com.chronos.core.modules.global.domain.records.Id;
 import br.com.chronos.server.database.jpa.collaborator.mappers.CollaboratorMapper;
 import br.com.chronos.server.database.jpa.collaborator.models.CollaboratorModel;
+import kotlin.Pair;
 
 interface JpaCollaboratorModelRepository extends JpaRepository<CollaboratorModel, UUID> {
 }
@@ -50,4 +53,12 @@ public class JpaCollaboratorRepository implements CollaboratorsRepository {
     repository.delete(collaboratorModel);
   }
 
+  @Override
+  public Pair<Collection<Collaborator>, Long> findMany(int page, int itemsPerPage) {
+    var pageable = PageRequest.of(page - 1, itemsPerPage);
+    var collaboratorModels = repository.findAll(pageable);
+    var items = collaboratorModels.getContent().stream().toList();
+    var itemsCount = collaboratorModels.getTotalElements();
+    return new Pair<>(Collection.createFrom(items, mapper::toEntity), itemsCount);
+  }
 }
