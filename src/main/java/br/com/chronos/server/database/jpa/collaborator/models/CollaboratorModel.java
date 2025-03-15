d43d148892f92f62a92ce9f4afe7d37be6f3cf9b
JohnPetros <joaopedro.nc@outlook.com>
2025-03-14 21:24:04 -0300

    ♻️ refactor: remove unnecessary setting of name param in some columns of CollaboratorModel

--- src/main/java/br/com/chronos/server/database/jpa/collaborator/models/CollaboratorModel.java
+++ src/main/java/br/com/chronos/server/database/jpa/collaborator/models/CollaboratorModel.java
@@ -1,51 +1,48 @@
 package br.com.chronos.server.database.jpa.collaborator.models;
 
-import java.util.UUID;
-
-import br.com.chronos.core.modules.collaboration.domain.records.CollaboratorRole.Role;
-import br.com.chronos.core.modules.collaboration.domain.records.CollaboratorSector.Sector;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import jakarta.persistence.Id;
+import java.util.UUID;
 
-@Entity
-@Table(name = "collaborators")
+import br.com.chronos.core.modules.collaboration.domain.records.CollaboratorRole.Role;
+import br.com.chronos.core.modules.collaboration.domain.records.CollaboratorSector.Sector;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-
-public class CollaboratorModel{
+@Entity
+@Table(name = "collaborators")
+public class CollaboratorModel {
     @Id
     private UUID id;
-    @Column(name = "name", nullable = false, length = 100)
+    @Column(nullable = false, length = 100)
     private String name;
-    @Column(name = "email", nullable = false, unique = true, length = 100)
+    @Column(nullable = false, unique = true, length = 100)
     private String email;
-    @Column(name = "cpf", nullable = false, unique = true, length = 11)
+    @Column(nullable = false, unique = true, length = 11)
     private String cpf;
-    @Column(name = "password", nullable = false, length = 20)
+    @Column(nullable = false, length = 20)
     private String password;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "sector", nullable = false)
+    @Column(nullable = false)
     private Sector sector;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "role", nullable = false)
+    @Column(nullable = false)
     private Role role;
 
-    @Column(name = "is_active", nullable = false)
-    private Boolean isActive;
+    @Column(nullable = false)
+    @Builder.Default
+    private Boolean isActive = true;
 
 }
