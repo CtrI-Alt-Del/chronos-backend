b70ba102093e2483a95f078c2964751c90b05f49
JohnPetros <joaopedro.nc@outlook.com>
2025-03-14 21:46:12 -0300

    ✨ use case: edit time punch schedule

---
+++ src/main/java/br/com/chronos/core/modules/work_schedule/use_cases/EditTimePunchScheduleUseCase.java
@@ -0,0 +1,32 @@
+package br.com.chronos.core.modules.work_schedule.use_cases;
+
+import br.com.chronos.core.modules.global.domain.records.Id;
+import br.com.chronos.core.modules.work_schedule.domain.dtos.TimePunchDto;
+import br.com.chronos.core.modules.work_schedule.domain.entities.TimePunch;
+import br.com.chronos.core.modules.work_schedule.domain.exceptions.TimePunchNotFoundException;
+import br.com.chronos.core.modules.work_schedule.interfaces.repositories.TimePunchesRepository;
+
+public class EditTimePunchScheduleUseCase {
+  private final TimePunchesRepository repository;
+
+  public EditTimePunchScheduleUseCase(TimePunchesRepository repository) {
+    this.repository = repository;
+  }
+
+  public TimePunchDto execute(TimePunchDto timePunchDto) {
+    var newTimePunch = new TimePunch(timePunchDto);
+    var oldTimePunch = findOldTimePunch(newTimePunch.getId());
+    oldTimePunch.replaceWith(newTimePunch);
+    repository.update(oldTimePunch);
+    return oldTimePunch.getDto();
+  }
+
+  public TimePunch findOldTimePunch(Id id) {
+    var timePunch = repository.findById(id);
+    if (timePunch.isEmpty()) {
+      throw new TimePunchNotFoundException();
+    }
+    return timePunch.get();
+  }
+
+}
