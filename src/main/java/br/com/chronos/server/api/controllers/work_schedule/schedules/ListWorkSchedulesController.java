4759a210e79f33a8d7e20289563c3d31d407fbb5
JohnPetros <joaopedro.nc@outlook.com>
2025-03-18 17:18:12 -0300

    📶 api: add ListWorkSchedulesController

--- src/main/java/br/com/chronos/server/api/controllers/work_schedule/schedules/ListWorkSchedulesController.java
+++ src/main/java/br/com/chronos/server/api/controllers/work_schedule/schedules/ListWorkSchedulesController.java
@@ -3,9 +3,9 @@ package br.com.chronos.server.api.controllers.work_schedule.schedules;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.ResponseEntity;
 
 import br.com.chronos.core.modules.work_schedule.domain.dtos.WorkScheduleDto;
 import br.com.chronos.core.modules.work_schedule.interfaces.repositories.WorkSchedulesRepository;
@@ -17,9 +17,9 @@ public class ListWorkSchedulesController {
   private WorkSchedulesRepository workSchedulesRepository;
 
   @GetMapping
-  public ResponseEntity<List<WorkScheduleDto>> handle() {
+  public ResponseEntity<List<WorkScheduleDto>> handle(@RequestParam int page) {
     var useCase = new ListWorkSchedulesUseCase(workSchedulesRepository);
-    var workdaySchedules = useCase.execute();
+    var workdaySchedules = useCase.execute(page);
     return ResponseEntity.ok(workdaySchedules);
   }
 }
