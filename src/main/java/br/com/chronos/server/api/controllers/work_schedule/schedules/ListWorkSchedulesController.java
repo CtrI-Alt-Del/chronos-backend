ac1e0721ae022e6df98864db3770372ec630f5da
JohnPetros <joaopedro.nc@outlook.com>
2025-03-18 20:38:20 -0300

    ♻️ refactor: remove useless imports across project

--- src/main/java/br/com/chronos/server/api/controllers/work_schedule/schedules/ListWorkSchedulesController.java
+++ src/main/java/br/com/chronos/server/api/controllers/work_schedule/schedules/ListWorkSchedulesController.java
@@ -3,7 +3,6 @@ package br.com.chronos.server.api.controllers.work_schedule.schedules;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
--- src/main/java/br/com/chronos/server/api/controllers/work_schedule/workday_logs/ReportWorkdayHistoryController.java
+++ src/main/java/br/com/chronos/server/api/controllers/work_schedule/workday_logs/ReportWorkdayHistoryController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import br.com.chronos.core.modules.work_schedule.domain.dtos.WorkdayLogDto;
