e30a5dca11c4013e9d89531d1513fc7f33235eda
JohnPetros <joaopedro.nc@outlook.com>
2025-03-17 23:26:14 -0300

    🌐 domain: add addMany to WorkdayLogsRepository interface

--- src/main/java/br/com/chronos/core/modules/work_schedule/interfaces/repositories/WorkdayLogsRepository.java
+++ src/main/java/br/com/chronos/core/modules/work_schedule/interfaces/repositories/WorkdayLogsRepository.java
@@ -4,8 +4,11 @@ import java.util.Optional;
 
 import br.com.chronos.core.modules.global.domain.records.Date;
 import br.com.chronos.core.modules.global.domain.records.Id;
+import br.com.chronos.core.modules.global.domain.records.Array;
 import br.com.chronos.core.modules.work_schedule.domain.entities.WorkdayLog;
 
 public interface WorkdayLogsRepository {
   Optional<WorkdayLog> findByCollaboratorAndDate(Id collaboratorId, Date date);
+
+  void addMany(Array<WorkdayLog> workdayLogs);
 }
