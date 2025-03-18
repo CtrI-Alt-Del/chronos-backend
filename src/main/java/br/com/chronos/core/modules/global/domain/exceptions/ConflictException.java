48b3ed222c180ce6bc9ed96c69abfad9952c6028
JohnPetros <joaopedro.nc@outlook.com>
2025-03-18 17:18:50 -0300

    🌐 domain: add ConflictException

---
+++ src/main/java/br/com/chronos/core/modules/global/domain/exceptions/ConflictException.java
@@ -0,0 +1,9 @@
+package br.com.chronos.core.modules.global.domain.exceptions;
+
+public class ConflictException extends AppException {
+
+  public ConflictException(String message) {
+    super("Conflic Exception", message);
+  }
+
+}
