1a76e7300ab8a0969afc142574a67e6ee52cfa18
JohnPetros <joaopedro.nc@outlook.com>
2025-03-18 10:28:04 -0300

    ✨ use case: list collaborator workday logs

--- src/main/java/br/com/chronos/server/database/jpa/work_schedule/repositories/JpaWorkdayLogsRepository.java
+++ src/main/java/br/com/chronos/server/database/jpa/work_schedule/repositories/JpaWorkdayLogsRepository.java
@@ -2,9 +2,10 @@ package br.com.chronos.server.database.jpa.work_schedule.repositories;
 
 import java.util.Optional;
 
+import br.com.chronos.core.modules.global.domain.records.Id;
 import br.com.chronos.core.modules.global.domain.records.Array;
 import br.com.chronos.core.modules.global.domain.records.Date;
-import br.com.chronos.core.modules.global.domain.records.Id;
+import br.com.chronos.core.modules.global.domain.records.DateRange;
 import br.com.chronos.core.modules.work_schedule.domain.entities.WorkdayLog;
 import br.com.chronos.core.modules.work_schedule.domain.records.Page;
 import br.com.chronos.core.modules.work_schedule.interfaces.repositories.WorkdayLogsRepository;
@@ -24,10 +25,14 @@ public class JpaWorkdayLogsRepository implements WorkdayLogsRepository {
   @Override
   public Array<WorkdayLog> findManyByCollaboratorAndDateRange(
       Id collaboratorId,
-      Date startDate,
-      Date endDate,
+      DateRange dateRange,
       Page page) {
     throw new UnsupportedOperationException("Unimplemented method 'findManyByCollaboratorAndDateRange'");
+  }
+
+  @Override
+  public Array<WorkdayLog> findManyByDate(Date date, Page page) {
+    throw new UnsupportedOperationException("Unimplemented method 'findManyByDate'");
   }
 
 }
