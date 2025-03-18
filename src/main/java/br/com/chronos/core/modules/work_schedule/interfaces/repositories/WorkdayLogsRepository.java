9a9fed238feb65ef61dd539a105f475a4fd60dcb
JohnPetros <joaopedro.nc@outlook.com>
2025-03-18 10:23:28 -0300

    📑 interfaces: add findManyByCollaboratorAndDateRange and findManyByDate methods to WorkdayLogsRepository

--- src/main/java/br/com/chronos/core/modules/work_schedule/interfaces/repositories/WorkdayLogsRepository.java
+++ src/main/java/br/com/chronos/core/modules/work_schedule/interfaces/repositories/WorkdayLogsRepository.java
@@ -3,6 +3,7 @@ package br.com.chronos.core.modules.work_schedule.interfaces.repositories;
 import java.util.Optional;
 
 import br.com.chronos.core.modules.global.domain.records.Date;
+import br.com.chronos.core.modules.global.domain.records.DateRange;
 import br.com.chronos.core.modules.global.domain.records.Id;
 import br.com.chronos.core.modules.global.domain.records.Array;
 import br.com.chronos.core.modules.work_schedule.domain.records.Page;
@@ -11,11 +12,9 @@ import br.com.chronos.core.modules.work_schedule.domain.entities.WorkdayLog;
 public interface WorkdayLogsRepository {
   Optional<WorkdayLog> findByCollaboratorAndDate(Id collaboratorId, Date date);
 
-  Array<WorkdayLog> findManyByCollaboratorAndDateRange(
-      Id collaboratorId,
-      Date startDate,
-      Date endDate,
-      Page page);
+  Array<WorkdayLog> findManyByCollaboratorAndDateRange(Id collaboratorId, DateRange dateRange, Page page);
+
+  Array<WorkdayLog> findManyByDate(Date date, Page page);
 
   void addMany(Array<WorkdayLog> workdayLogs);
 }
