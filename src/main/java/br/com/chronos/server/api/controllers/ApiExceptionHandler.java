6d4a44ae99b583dd3d1433ee12718985516bf9f8
kaufon <kauanfvale@gmail.com>
2025-03-18 16:37:44 -0300

    🛜api: add exception handler controller

---
+++ src/main/java/br/com/chronos/server/api/controllers/ApiExceptionHandler.java
@@ -0,0 +1,43 @@
+package br.com.chronos.server.api.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import br.com.chronos.core.modules.global.domain.exceptions.AppException;
+import br.com.chronos.core.modules.global.domain.exceptions.NotFoundException;
+import br.com.chronos.core.modules.global.domain.exceptions.ValidationException;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+  @Data
+  @AllArgsConstructor
+  private static class ExceptionMessage {
+    private String title;
+    private String message;
+  }
+  
+  @ExceptionHandler(NotFoundException.class)
+  private ResponseEntity<ExceptionMessage> handleNotFoundException(NotFoundException exception){
+    var message = new ExceptionMessage(exception.getTitle(),exception.getMessage());
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message);
+  }
+  @ExceptionHandler(RuntimeException.class)
+  private ResponseEntity<ExceptionMessage> handleRunTimeException(RuntimeException exception){
+    var message = new ExceptionMessage("Run time exception",exception.getMessage());
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message);
+  }
+  @ExceptionHandler(ValidationException.class)
+  private ResponseEntity<ExceptionMessage> handleValidationException(ValidationException exception){
+    var message = new ExceptionMessage(exception.getTitle(),exception.getMessage());
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message);
+  }
+  @ExceptionHandler(AppException.class)
+  private ResponseEntity<ExceptionMessage> handleAppException(AppException exception){
+    var message = new ExceptionMessage(exception.getTitle(),exception.getMessage());
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message);
+  }
+}
