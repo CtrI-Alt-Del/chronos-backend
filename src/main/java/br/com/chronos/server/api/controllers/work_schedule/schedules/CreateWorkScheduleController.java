414df38ad9e14fefbeb246916f0bdb2110f67c95
JohnPetros <joaopedro.nc@outlook.com>
2025-03-18 20:38:51 -0300

    🐛 fix: set PostMapping to CreateWorkScheduleController.handle

--- src/main/java/br/com/chronos/server/api/controllers/work_schedule/schedules/CreateWorkScheduleController.java
+++ src/main/java/br/com/chronos/server/api/controllers/work_schedule/schedules/CreateWorkScheduleController.java
@@ -3,6 +3,7 @@ package br.com.chronos.server.api.controllers.work_schedule.schedules;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import br.com.chronos.core.modules.work_schedule.domain.dtos.WorkScheduleDto;
@@ -15,6 +16,7 @@ public class CreateWorkScheduleController {
   @Autowired
   private WorkSchedulesRepository workSchedulesRepository;
 
+  @PostMapping
   public ResponseEntity<WorkScheduleDto> handle(@RequestBody WorkScheduleDto body) {
     var useCase = new CreateWorkScheduleUseCase(workSchedulesRepository);
     var workScheduleDto = useCase.execute(body);
