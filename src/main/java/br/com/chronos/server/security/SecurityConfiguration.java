7afb092f3f88678c369cdbbcfcada4fc9e69bffe
kaufon <kauanfvale@gmail.com>
2025-03-19 17:15:50 -0300

    ⚙️configuration: make security configuration

--- src/main/java/br/com/chronos/server/security/SecurityConfiguration.java
+++ src/main/java/br/com/chronos/server/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package br.com.chronos.server.security;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -13,13 +14,23 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import br.com.chronos.core.modules.global.interfaces.providers.EnvProvider;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+  @Autowired
+  private SecurityJwtFilter securityJwtFilter;
+
+  @Autowired
+  private EnvProvider dotenvProvider;
+
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
     return httpSecurity
@@ -27,8 +38,10 @@ public class SecurityConfiguration {
         .cors(cors -> cors.configurationSource(configurationSource()))
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(auth -> auth
-            .anyRequest().permitAll() // Permits all requests
-        )
+            .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+            .requestMatchers("/collaborator").hasRole("MANAGER")
+            .anyRequest().authenticated())
+        .addFilterBefore(securityJwtFilter, UsernamePasswordAuthenticationFilter.class)
         .build();
   }
 
@@ -36,8 +49,13 @@ public class SecurityConfiguration {
   CorsConfigurationSource configurationSource() {
     var source = new UrlBasedCorsConfigurationSource();
     CorsConfiguration defaultConfig = new CorsConfiguration();
-    // Todo: change the allowed origin to .env variable
-    defaultConfig.setAllowedOrigins(List.of("http://localhost:3000"));
+
+    String allowedOrigin = dotenvProvider.get("WEB_APP_URL");
+    if (allowedOrigin == null || allowedOrigin.isEmpty()) {
+      throw new IllegalStateException("Missing required environment for variable for Web App Url ");
+    }
+
+    defaultConfig.setAllowedOrigins(List.of(allowedOrigin));
     defaultConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
     defaultConfig.setAllowedHeaders(List.of("*"));
     source.registerCorsConfiguration("/**", defaultConfig);
@@ -45,12 +63,12 @@ public class SecurityConfiguration {
   }
 
   @Bean
-  AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+  AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
     return configuration.getAuthenticationManager();
   }
 
   @Bean
-  PasswordEncoder passwordEncoder(){
+  PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
 
