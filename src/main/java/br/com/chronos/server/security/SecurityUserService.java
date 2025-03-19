3e4f58539cc4c9e571252a4c92ce8f91588db6e0
kaufon <kauanfvale@gmail.com>
2025-03-19 16:18:50 -0300

    ♻️refactor: make security user service use account instead of collaborator

--- src/main/java/br/com/chronos/server/security/SecurityUserService.java
+++ src/main/java/br/com/chronos/server/security/SecurityUserService.java
@@ -6,27 +6,28 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import br.com.chronos.core.modules.auth.domain.entities.Account;
 import br.com.chronos.core.modules.auth.domain.exceptions.NotAuthenticatedException;
+import br.com.chronos.core.modules.auth.interfaces.repositories.AccountsRepository;
 import br.com.chronos.core.modules.auth.use_cases.GetAccountUseCase;
-import br.com.chronos.core.modules.collaboration.domain.entities.Collaborator;
-import br.com.chronos.core.modules.collaboration.interfaces.repositories.CollaboratorsRepository;
 
 @Service
 public class SecurityUserService implements UserDetailsService {
 
   @Autowired
-  private CollaboratorsRepository collaboratorsRepository;
+  private AccountsRepository accountsRepository;
 
   @Override
   public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
     try {
-      var useCase = new GetAccountUseCase(collaboratorsRepository);
-      var collaboratorDto = useCase.execute(email);
-      var collaborator = new Collaborator(collaboratorDto);
-      var securityUser = new SecurityUser(collaborator);
+      var useCase = new GetAccountUseCase(accountsRepository);
+      var accountDto = useCase.execute(email);
+      var account = new Account(accountDto);
+      var securityUser = new SecurityUser(account);
       return securityUser;
     } catch (Exception e) {
-      throw new NotAuthenticatedException(e.getMessage());
+      System.out.println(e);
+      throw new NotAuthenticatedException("Credenciais invalidas");
     }
   }
 }
