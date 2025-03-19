95dba9d6673d097759482f71e1d6adfce50f4e0d
kaufon <kauanfvale@gmail.com>
2025-03-19 16:19:10 -0300

    🌐domain: add account entity

---
+++ src/main/java/br/com/chronos/core/modules/auth/domain/entities/Account.java
@@ -0,0 +1,73 @@
+package br.com.chronos.core.modules.auth.domain.entities;
+
+import br.com.chronos.core.modules.global.domain.abstracts.Entity;
+import br.com.chronos.core.modules.global.domain.records.Email;
+import br.com.chronos.core.modules.global.domain.records.Logical;
+import br.com.chronos.core.modules.auth.domain.dtos.AccountDto;
+import br.com.chronos.core.modules.collaboration.domain.records.CollaboratorRole;
+import br.com.chronos.core.modules.collaboration.domain.records.CollaboratorSector;
+import br.com.chronos.core.modules.collaboration.domain.records.Password;
+
+public final class Account extends Entity {
+  private Email email;
+  private Password password;
+  private Logical isActive;
+  private CollaboratorRole role;
+  private CollaboratorSector sector;
+
+  public Account(AccountDto dto) {
+    super(dto.id);
+    email = Email.create(dto.email, "Collaborator email");
+    password = Password.create(dto.password, "Collaborator password");
+    role = CollaboratorRole.create(dto.role);
+    sector = CollaboratorSector.create(dto.sector);
+    isActive = (dto.isActive != null) ? Logical.create(dto.isActive) : Logical.create(true);
+
+  }
+  public Email getEmail() {
+    return email;
+  }
+
+  public Password getPassword() {
+    return password;
+  }
+
+  public Logical getIsActive() {
+    return isActive;
+  }
+
+  public void updateEmail(Email email) {
+    this.email = email;
+  }
+
+  public void updatePassword(Password password) {
+    this.password = password;
+  }
+
+  public void disable() {
+    this.isActive = Logical.create(false);
+  }
+
+  public void enable() {
+    this.isActive = Logical.create(true);
+  }
+
+  public CollaboratorRole getRole() {
+    return role;
+  }
+
+  public CollaboratorSector getSector() {
+    return sector;
+  }
+
+  public AccountDto getDto() {
+    var dto = new AccountDto()
+        .setId(getId().value().toString())
+        .setEmail(getEmail().value().toString())
+        .setPassword(getPassword().value().toString())
+        .setActive(getIsActive().value())
+        .setRole(getRole().value().toString())
+        .setSector(getSector().value().toString());
+    return dto;
+  }
+}
