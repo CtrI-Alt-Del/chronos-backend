3aa1d2b95dfc6dcf9456104187a58ad16e1e3bf3
kaufon <kauanfvale@gmail.com>
2025-03-21 08:07:52 -0300

    🐛fix: move get profile use case to collaborator domain

--- src/main/java/br/com/chronos/server/api/controllers/auth/GetProfileController.java
+++ src/main/java/br/com/chronos/server/api/controllers/auth/GetProfileController.java
@@ -4,24 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import br.com.chronos.core.modules.auth.interfaces.repositories.AccountsRepository;
-import br.com.chronos.core.modules.auth.use_cases.GetProfileUseCase;
 import br.com.chronos.core.modules.collaboration.domain.dtos.CollaboratorDto;
+import br.com.chronos.core.modules.collaboration.interfaces.repositories.CollaboratorsRepository;
+import br.com.chronos.core.modules.collaboration.use_cases.GetProfileUseCase;
 import br.com.chronos.core.modules.global.interfaces.providers.AuthenticationProvider;
 
 @AuthController
-public class GetProfileController{
+public class GetProfileController {
 
   @Autowired
-  private AccountsRepository repository;
+  private CollaboratorsRepository repository;
   @Autowired
   private AuthenticationProvider authenticationProvider;
 
   @GetMapping("/profile")
-  public ResponseEntity<CollaboratorDto> handle(){
+  public ResponseEntity<CollaboratorDto> handle() {
     var useCase = new GetProfileUseCase(repository);
     var responsible = authenticationProvider.getAuthenticatedUser();
-    var response = useCase.execute(responsible);
+    var response = useCase.execute(responsible.getEmail());
     return ResponseEntity.ok(response);
 
   }
