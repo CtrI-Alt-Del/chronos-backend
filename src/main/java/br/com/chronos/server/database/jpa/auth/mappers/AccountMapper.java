c126e47b4ccdf3871a4b2867a71beed18e3a4653
kaufon <kauanfvale@gmail.com>
2025-03-21 08:06:39 -0300

    🐛fix: accounts mapper to entity method

--- src/main/java/br/com/chronos/server/database/jpa/auth/mappers/AccountMapper.java
+++ src/main/java/br/com/chronos/server/database/jpa/auth/mappers/AccountMapper.java
@@ -4,13 +4,11 @@ import org.springframework.stereotype.Service;
 
 import br.com.chronos.core.modules.auth.domain.dtos.AccountDto;
 import br.com.chronos.core.modules.auth.domain.entities.Account;
-import br.com.chronos.core.modules.collaboration.domain.dtos.CollaboratorDto;
-import br.com.chronos.core.modules.collaboration.domain.entities.Collaborator;
 import br.com.chronos.server.database.jpa.collaborator.models.CollaboratorModel;
 
 @Service
 public class AccountMapper {
-  public Account toAccount(CollaboratorModel model) {
+  public Account toEntity(CollaboratorModel model) {
     var dto = new AccountDto()
         .setId(model.getId().toString())
         .setEmail(model.getEmail().toString())
@@ -20,18 +18,5 @@ public class AccountMapper {
         .setSector(model.getSector().toString());
     return new Account(dto);
 
-  }
-
-  public Collaborator toCollaborator(CollaboratorModel model) {
-    var dto = new CollaboratorDto()
-        .setId(model.getId().toString())
-        .setPassword(model.getPassword().toString())
-        .setName(model.getName().toString())
-        .setEmail(model.getEmail().toString())
-        .setCpf(model.getCpf().toString())
-        .setSector(model.getSector().toString())
-        .setRole(model.getRole().toString())
-        .setActive(model.getIsActive());
-    return new Collaborator(dto);
   }
 }
