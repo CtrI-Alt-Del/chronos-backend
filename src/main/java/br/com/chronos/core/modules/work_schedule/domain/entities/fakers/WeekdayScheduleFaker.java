ab469a27c7fa99f51b104d086c82a70c34b41981
JohnPetros <joaopedro.nc@outlook.com>
2025-03-22 15:42:46 -0300

    🥸 fakers: add WeekdayScheduleFaker

---
+++ src/main/java/br/com/chronos/core/modules/work_schedule/domain/entities/fakers/WeekdayScheduleFaker.java
@@ -0,0 +1,19 @@
+package br.com.chronos.core.modules.work_schedule.domain.entities.fakers;
+
+import br.com.chronos.core.modules.global.domain.records.fakers.IdFaker;
+import br.com.chronos.core.modules.work_schedule.domain.dtos.WeekdayScheduleDto;
+import br.com.chronos.core.modules.work_schedule.domain.entities.WeekdaySchedule;
+import br.com.chronos.core.modules.work_schedule.domain.records.fakers.WeekdayFaker;
+
+public class WeekdayScheduleFaker {
+  public static WeekdaySchedule fake() {
+    return new WeekdaySchedule(fakeDto());
+  }
+
+  public static WeekdayScheduleDto fakeDto() {
+    return new WeekdayScheduleDto()
+        .setId(IdFaker.fakeDto())
+        .setTimePunch(TimePunchFaker.fakeDto())
+        .setWeekday(WeekdayFaker.fakeDto());
+  }
+}
