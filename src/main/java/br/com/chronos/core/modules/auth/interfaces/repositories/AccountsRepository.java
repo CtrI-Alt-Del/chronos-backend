5c1c12882768e79e376f61b3a57765a527b2cf01
JohnPetros <joaopedro.nc@outlook.com>
2025-03-23 13:52:54 -0300

    💾 database: implement addMany method of AccountsRepository using JPA

--- src/main/java/br/com/chronos/core/modules/auth/interfaces/repositories/AccountsRepository.java
+++ src/main/java/br/com/chronos/core/modules/auth/interfaces/repositories/AccountsRepository.java
@@ -3,8 +3,13 @@ package br.com.chronos.core.modules.auth.interfaces.repositories;
 import java.util.Optional;
 
 import br.com.chronos.core.modules.auth.domain.entities.Account;
+import br.com.chronos.core.modules.global.domain.records.Array;
 import br.com.chronos.core.modules.global.domain.records.Email;
 
-public interface AccountsRepository{
+public interface AccountsRepository {
+  void add(Account account);
+
+  void addMany(Array<Account> accounts);
+
   Optional<Account> findByEmail(Email email);
 }
--- src/main/java/br/com/chronos/server/database/jpa/auth/repositories/JpaAccountsRepository.java
+++ src/main/java/br/com/chronos/server/database/jpa/auth/repositories/JpaAccountsRepository.java
@@ -8,22 +8,34 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.chronos.core.modules.auth.domain.entities.Account;
 import br.com.chronos.core.modules.auth.interfaces.repositories.AccountsRepository;
+import br.com.chronos.core.modules.global.domain.records.Array;
 import br.com.chronos.core.modules.global.domain.records.Email;
 import br.com.chronos.server.database.jpa.auth.mappers.AccountMapper;
+import br.com.chronos.server.database.jpa.auth.models.AccountModel;
 import br.com.chronos.server.database.jpa.collaborator.models.CollaboratorModel;
 
-// Cant name this to JpaCollaboratorModelsRepository,spring will complain about it
-interface JpaAccountModelsRepository extends JpaRepository<CollaboratorModel, UUID> {
-  public Optional<CollaboratorModel> findByEmail(String email);
+interface JpaAccountModelsRepository extends JpaRepository<AccountModel, UUID> {
+  public Optional<AccountModel> findByEmail(String email);
 }
 
 public class JpaAccountsRepository implements AccountsRepository {
-
   @Autowired
   JpaAccountModelsRepository repository;
 
   @Autowired
   AccountMapper mapper;
+
+  @Override
+  public void add(Account account) {
+    var accountModel = toModel(account);
+    repository.save(accountModel);
+  }
+
+  @Override
+  public void addMany(Array<Account> accounts) {
+    var accountModels = accounts.map(this::toModel);
+    repository.saveAll(accountModels.list());
+  }
 
   @Override
   public Optional<Account> findByEmail(Email email) {
@@ -34,6 +46,18 @@ public class JpaAccountsRepository implements AccountsRepository {
     }
     var account = mapper.toEntity(collaboratorModel.get());
     return Optional.of(account);
+  }
+
+  private AccountModel toModel(Account account) {
+    var accountModel = mapper.toModel(account);
+    if (account.isFromCollaborator().isTrue()) {
+      var collaboratorModel = CollaboratorModel
+          .builder()
+          .id(account.getCollaboratorId().value())
+          .build();
+      accountModel.setCollaborator(collaboratorModel);
+    }
+    return accountModel;
   }
 
 }
