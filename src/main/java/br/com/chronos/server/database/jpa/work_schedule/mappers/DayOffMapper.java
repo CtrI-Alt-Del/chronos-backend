24c4c135a4af7e7745abab71351b204c9125f8ac
JohnPetros <joaopedro.nc@outlook.com>
2025-03-23 13:12:35 -0300

    💾 database: add DayOffMapper

---
+++ src/main/java/br/com/chronos/server/database/jpa/work_schedule/mappers/DayOffMapper.java
@@ -0,0 +1,28 @@
+package br.com.chronos.server.database.jpa.work_schedule.mappers;
+
+import java.time.LocalDate;
+
+import org.springframework.stereotype.Component;
+
+import br.com.chronos.core.modules.global.domain.records.Date;
+import br.com.chronos.core.modules.global.domain.records.Id;
+import br.com.chronos.server.database.jpa.work_schedule.models.DayOffModel;
+
+@Component
+public class DayOffMapper {
+  public DayOffModel toModel(Date dayOff) {
+    return DayOffModel
+        .builder()
+        .id(Id.random().value())
+        .date(dayOff.value())
+        .build();
+  }
+
+  public Date toRecord(DayOffModel dayOff) {
+    return Date.create(dayOff.getDate());
+  }
+
+  public LocalDate toDto(DayOffModel model) {
+    return model.getDate();
+  }
+}
