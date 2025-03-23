901a24c480d6e60fcc3f6eedf14aae79bf7eb2a4
JohnPetros <joaopedro.nc@outlook.com>
2025-03-23 13:38:57 -0300

    🥸 fakers: add workday log faker

---
+++ src/main/java/br/com/chronos/core/modules/work_schedule/domain/entities/fakers/WorkdayLogFaker.java
@@ -0,0 +1,34 @@
+package br.com.chronos.core.modules.work_schedule.domain.entities.fakers;
+
+import br.com.chronos.core.modules.global.domain.entities.fakers.ResponsibleFaker;
+import br.com.chronos.core.modules.global.domain.records.fakers.DateFaker;
+import br.com.chronos.core.modules.global.domain.records.fakers.IdFaker;
+import br.com.chronos.core.modules.work_schedule.domain.dtos.WorkdayLogDto;
+import br.com.chronos.core.modules.work_schedule.domain.entities.WorkdayLog;
+import br.com.chronos.core.modules.global.domain.records.Array;
+import br.com.chronos.core.modules.work_schedule.domain.records.fakers.WorkdayStatusFaker;
+
+public class WorkdayLogFaker {
+  public static WorkdayLog fake() {
+    return new WorkdayLog(fakeDto());
+  }
+
+  public static WorkdayLogDto fakeDto() {
+    var fakeResponsibleDto = ResponsibleFaker.fakeDto();
+    return new WorkdayLogDto()
+        .setId(IdFaker.fakeDto())
+        .setDate(DateFaker.fakeDto())
+        .setTimePunchLog(TimePunchFaker.fakeDto())
+        .setTimePunchSchedule(TimePunchFaker.fakeDto())
+        .setStatus(WorkdayStatusFaker.fakeDto())
+        .setResponsible(fakeResponsibleDto.id, fakeResponsibleDto);
+  }
+
+  public static Array<WorkdayLog> fakeMany(int count) {
+    Array<WorkdayLog> fakeWordayLogs = Array.createAsEmpty();
+    for (var index = 0; index < count; index++) {
+      fakeWordayLogs.add(fake());
+    }
+    return fakeWordayLogs;
+  }
+}
