a929c08b94c31a1bae6b8df314a05b07d4722ac9
JohnPetros <joaopedro.nc@outlook.com>
2025-03-23 13:32:46 -0300

    🥸 fakers: add collaborator faker

--- src/main/java/br/com/chronos/core/modules/collaboration/domain/entities/Collaborator.java
+++ src/main/java/br/com/chronos/core/modules/collaboration/domain/entities/Collaborator.java
@@ -1,33 +1,41 @@
 package br.com.chronos.core.modules.collaboration.domain.entities;
 
 import br.com.chronos.core.modules.collaboration.domain.dtos.CollaboratorDto;
-import br.com.chronos.core.modules.collaboration.domain.records.CollaboratorRole;
-import br.com.chronos.core.modules.collaboration.domain.records.CollaboratorSector;
-import br.com.chronos.core.modules.collaboration.domain.records.Password;
 import br.com.chronos.core.modules.global.domain.abstracts.Entity;
+import br.com.chronos.core.modules.global.domain.records.CollaborationSector;
 import br.com.chronos.core.modules.global.domain.records.Cpf;
 import br.com.chronos.core.modules.global.domain.records.Email;
 import br.com.chronos.core.modules.global.domain.records.Logical;
+import br.com.chronos.core.modules.global.domain.records.Role;
 import br.com.chronos.core.modules.global.domain.records.Text;
 
 public final class Collaborator extends Entity {
   private Text name;
   private Email email;
-  private Password password;
   private Cpf cpf;
-  private CollaboratorRole role;
-  private CollaboratorSector sector;
+  private Role role;
+  private CollaborationSector sector;
   private Logical isActive;
 
   public Collaborator(CollaboratorDto dto) {
     super(dto.id);
-    name = Text.create(dto.name, "Collaborator name");
-    email = Email.create(dto.email, "Collaborator email");
-    password = Password.create(dto.password, "Collaborator password");
-    cpf = Cpf.create(dto.cpf, "Collaborator cpf");
-    role = CollaboratorRole.create(dto.role);
-    sector = CollaboratorSector.create(dto.sector);
+    name = Text.create(dto.name, "nome do colaborador");
+    email = Email.create(dto.email);
+    cpf = Cpf.create(dto.cpf);
+    role = Role.create(dto.role);
+    sector = CollaborationSector.create(dto.sector);
     isActive = (dto.isActive != null) ? Logical.create(dto.isActive) : Logical.create(true);
+  }
+
+  public Logical isFromSameSector(Collaborator collaborator) {
+    if (collaborator == null) {
+      return Logical.createAsFalse();
+    }
+    if (this.role.isAdmin().isTrue()) {
+      return Logical.createAsTrue();
+    }
+
+    return Logical.create(this.getSector().value().equals(collaborator.getSector().value()));
   }
 
   public Text getName() {
@@ -38,19 +46,15 @@ public final class Collaborator extends Entity {
     return email;
   }
 
-  public Password getPassword() {
-    return password;
-  }
-
   public Cpf getCpf() {
     return cpf;
   }
 
-  public CollaboratorRole getRole() {
+  public Role getRole() {
     return role;
   }
 
-  public CollaboratorSector getSector() {
+  public CollaborationSector getSector() {
     return sector;
   }
 
@@ -63,19 +67,16 @@ public final class Collaborator extends Entity {
       this.name = Text.create(dto.name, "Collaborator name");
     }
     if (dto.email != null) {
-      this.email = Email.create(dto.email, "Collaborator email");
-    }
-    if (dto.password != null) {
-      this.password = Password.create(dto.password, "Collaborator password");
+      this.email = Email.create(dto.email);
     }
     if (dto.cpf != null) {
-      this.cpf = Cpf.create(dto.cpf, "Collaborator cpf");
+      this.cpf = Cpf.create(dto.cpf);
     }
     if (dto.role != null) {
-      this.role = CollaboratorRole.create(dto.role);
+      this.role = Role.create(dto.role);
     }
     if (dto.sector != null) {
-      this.sector = CollaboratorSector.create(dto.sector);
+      this.sector = CollaborationSector.create(dto.sector);
     }
     if (dto.isActive != null) {
       this.isActive = Logical.create(dto.isActive);
@@ -90,23 +91,11 @@ public final class Collaborator extends Entity {
     this.isActive = Logical.create(true);
   }
 
-  public Logical isFromSameSector(Collaborator otherAccount) {
-    if (otherAccount == null) {
-      return Logical.createAsFalse();
-    }
-    if (this.role.isAdmin().isTrue()) {
-      return Logical.createAsTrue();
-    }
-
-    return Logical.create(this.getSector().value().equals(otherAccount.getSector().value()));
-  }
-
   public CollaboratorDto getDto() {
     var dto = new CollaboratorDto()
         .setId(getId().value().toString())
         .setName(getName().value().toString())
         .setEmail(getEmail().value().toString())
-        .setPassword(getPassword().value().toString())
         .setCpf(getCpf().value().toString())
         .setRole(getRole().value().toString())
         .setSector(getSector().value().toString())
