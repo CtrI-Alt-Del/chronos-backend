bdfa32e6d385502c50ca1e18c8ef941c3cc9cf1d
JohnPetros <joaopedro.nc@outlook.com>
2025-03-23 13:24:40 -0300

    ⚙️ config: implement database seed as spring command line runner

--- src/main/java/br/com/chronos/server/ServerApplication.java
+++ src/main/java/br/com/chronos/server/ServerApplication.java
@@ -9,7 +9,6 @@ import br.com.chronos.server.providers.env.DotenvProvider;
 @SpringBootApplication
 @EnableScheduling
 public class ServerApplication {
-
   public static void main(String[] args) {
     DotenvProvider dotenvProvider = new DotenvProvider();
     System.setProperty("DATABASE_SOURCE_URL", dotenvProvider.get("DATABASE_SOURCE_URL"));
