5eb49ce551c688ed0893616b9011879ec21424d0
JohnPetros <joaopedro.nc@outlook.com>
2025-03-23 14:05:39 -0300

    🐛 fix: ensure each time punch props is correclty mapping to a model

--- src/main/java/br/com/chronos/server/database/jpa/work_schedule/mappers/TimePunchMapper.java
+++ src/main/java/br/com/chronos/server/database/jpa/work_schedule/mappers/TimePunchMapper.java
@@ -1,21 +1,19 @@
 package br.com.chronos.server.database.jpa.work_schedule.mappers;
 
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import br.com.chronos.core.modules.work_schedule.domain.dtos.TimePunchDto;
 import br.com.chronos.core.modules.work_schedule.domain.entities.TimePunch;
 import br.com.chronos.server.database.jpa.work_schedule.models.TimePunchModel;
 
-
-// Todo: tell petros about idea of using mapstruct
-@Service
+@Component
 public class TimePunchMapper {
   public TimePunchModel toModel(TimePunch entity) {
     var model = TimePunchModel.builder()
         .id(entity.getId().value())
         .firstClockIn(entity.getFirstClockIn().value())
-        .firstClockOut(entity.getSecondClockOut().value())
-        .seconcClockIn(entity.getSecondClockIn().value())
+        .firstClockOut(entity.getFirstClockOut().value())
+        .secondClockIn(entity.getSecondClockIn().value())
         .secondClockOut(entity.getSecondClockOut().value())
         .build();
 
@@ -27,7 +25,7 @@ public class TimePunchMapper {
         .setId(model.getId().toString())
         .setFirstClockOut(model.getFirstClockIn())
         .setFirstClockOut(model.getFirstClockOut())
-        .setSecondClockIn(model.getSeconcClockIn())
+        .setSecondClockIn(model.getSecondClockIn())
         .setSecondClockOut(model.getSecondClockOut());
 
     return new TimePunch(dto);
@@ -38,7 +36,7 @@ public class TimePunchMapper {
         .setId(model.getId().toString())
         .setFirstClockIn(model.getFirstClockIn())
         .setFirstClockOut(model.getFirstClockOut())
-        .setSecondClockIn(model.getSeconcClockIn())
+        .setSecondClockIn(model.getSecondClockIn())
         .setSecondClockOut(model.getSecondClockOut());
   }
 
