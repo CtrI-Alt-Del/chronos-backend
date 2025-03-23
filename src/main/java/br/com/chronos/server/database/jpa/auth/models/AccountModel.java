8c759651f38222a0d64287b6fcbf115435850eb9
JohnPetros <joaopedro.nc@outlook.com>
2025-03-23 13:51:36 -0300

    🌐 domain: add account model

---
+++ src/main/java/br/com/chronos/server/database/jpa/auth/models/AccountModel.java
@@ -0,0 +1,49 @@
+package br.com.chronos.server.database.jpa.auth.models;
+
+import jakarta.persistence.Table;
+
+import java.util.UUID;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import br.com.chronos.core.modules.global.domain.records.Role.RoleName;
+import br.com.chronos.server.database.jpa.collaborator.models.CollaboratorModel;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "accounts")
+public class AccountModel {
+  @Id
+  private UUID id;
+
+  @Column(nullable = false, unique = true, length = 100)
+  private String email;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column(nullable = false)
+  @Builder.Default
+  private Boolean isActive = true;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private RoleName role;
+
+  @OneToOne
+  @JoinColumn(name = "collaborator_id", nullable = true)
+  private CollaboratorModel collaborator;
+}
