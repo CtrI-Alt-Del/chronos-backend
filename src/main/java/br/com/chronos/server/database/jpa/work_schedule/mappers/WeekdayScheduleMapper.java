58f9ab6901f61f19eb582a7ccb566273f54b89ea
JohnPetros <joaopedro.nc@outlook.com>
2025-03-23 14:06:00 -0300

    💾 database: add weekday schedule mapper

---
+++ src/main/java/br/com/chronos/server/database/jpa/work_schedule/mappers/WeekdayScheduleMapper.java
@@ -0,0 +1,31 @@
+package br.com.chronos.server.database.jpa.work_schedule.mappers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import br.com.chronos.core.modules.global.domain.records.Id;
+import br.com.chronos.core.modules.work_schedule.domain.dtos.WeekdayScheduleDto;
+import br.com.chronos.core.modules.work_schedule.domain.entities.WeekdaySchedule;
+import br.com.chronos.server.database.jpa.work_schedule.models.WeekdayScheduleModel;
+
+@Component
+public class WeekdayScheduleMapper {
+  @Autowired
+  private TimePunchMapper timePuchMapper;
+
+  public WeekdayScheduleModel toModel(WeekdaySchedule entity) {
+    return WeekdayScheduleModel
+        .builder()
+        .id(Id.random().value())
+        .weekdayName(entity.getWeekday().name())
+        .timePunch(timePuchMapper.toModel(entity.getTimePunch()))
+        .build();
+  }
+
+  public WeekdayScheduleDto toDto(WeekdayScheduleModel model) {
+    return new WeekdayScheduleDto()
+        .setId(model.getId().toString())
+        .setWeekday(model.getWeekdayName().toString().toLowerCase())
+        .setTimePunch(timePuchMapper.toDto(model.getTimePunch()));
+  }
+}
