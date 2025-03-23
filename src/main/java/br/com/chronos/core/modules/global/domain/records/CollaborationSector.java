8856b13997198ec43b08bd8e956bac3e3b03ec01
JohnPetros <joaopedro.nc@outlook.com>
2025-03-23 13:37:04 -0300

    🥸 fakers: add collaboration sector faker

---
+++ src/main/java/br/com/chronos/core/modules/global/domain/records/CollaborationSector.java
@@ -0,0 +1,44 @@
+package br.com.chronos.core.modules.global.domain.records;
+
+import br.com.chronos.core.modules.global.domain.exceptions.ValidationException;
+
+public record CollaborationSector(Sector value) {
+  public enum Sector {
+    PRODUCTION,
+    COMERCIAL,
+    ADMINISTRATIVE,
+    HUMAN_RESOURCES
+  }
+
+  public static CollaborationSector create(String sector) {
+    if (sector == null) {
+      return new CollaborationSector(Sector.PRODUCTION);
+    }
+    var text = Text.create(sector.toUpperCase(), "setor");
+    try {
+      return new CollaborationSector(Sector.valueOf(text.value()));
+    } catch (Exception e) {
+      throw new ValidationException(text.key(), "deve ser do setor produtivo, comercial ou administrativo");
+    }
+  }
+
+  public String toString() {
+    return value.toString().toLowerCase();
+  }
+
+  public Logical isFromProduction() {
+    return Logical.create(value == Sector.PRODUCTION);
+  }
+
+  public Logical isFromComercial() {
+    return Logical.create(value == Sector.COMERCIAL);
+  }
+
+  public Logical isFromAdministrative() {
+    return Logical.create(value == Sector.ADMINISTRATIVE);
+  }
+
+  public Logical isFromHumanResources() {
+    return Logical.create(value == Sector.HUMAN_RESOURCES);
+  }
+}
