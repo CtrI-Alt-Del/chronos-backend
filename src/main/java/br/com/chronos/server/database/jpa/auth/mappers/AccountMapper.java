3ad27ae1de2d5a29d01c06b36552bc5f5d6feecc
JohnPetros <joaopedro.nc@outlook.com>
2025-03-23 13:52:01 -0300

    💾 database: add account mapper

--- src/main/java/br/com/chronos/server/database/jpa/auth/mappers/AccountMapper.java
+++ src/main/java/br/com/chronos/server/database/jpa/auth/mappers/AccountMapper.java
@@ -4,19 +4,30 @@ import org.springframework.stereotype.Service;
 
 import br.com.chronos.core.modules.auth.domain.dtos.AccountDto;
 import br.com.chronos.core.modules.auth.domain.entities.Account;
-import br.com.chronos.server.database.jpa.collaborator.models.CollaboratorModel;
+import br.com.chronos.server.database.jpa.auth.models.AccountModel;
 
 @Service
 public class AccountMapper {
-  public Account toEntity(CollaboratorModel model) {
+  public AccountModel toModel(Account entity) {
+    var model = AccountModel.builder()
+        .id(entity.getId().value())
+        .email(entity.getEmail().value())
+        .password(entity.getPassword().value())
+        .isActive(entity.getIsActive().value())
+        .role(entity.getRole().value())
+        .build();
+
+    return model;
+  }
+
+  public Account toEntity(AccountModel model) {
     var dto = new AccountDto()
         .setId(model.getId().toString())
         .setEmail(model.getEmail().toString())
         .setPassword(model.getPassword().toString())
         .setActive(model.getIsActive())
-        .setRole(model.getRole().toString())
-        .setSector(model.getSector().toString());
-    return new Account(dto);
+        .setRole(model.getRole().toString());
 
+    return new Account(dto);
   }
 }
