1dc8c33fe38da3c3f94c6c980cd059e7770e067a
kaufon <kauanfvale@gmail.com>
2025-03-24 18:11:25 -0300

    ♻️refactor: create collaborator controller

--- src/main/java/br/com/chronos/server/api/controllers/collaboration/collaborators/CreateCollaboratorController.java
+++ src/main/java/br/com/chronos/server/api/controllers/collaboration/collaborators/CreateCollaboratorController.java
@@ -6,25 +6,41 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import br.com.chronos.core.modules.auth.interfaces.repositories.AccountsRepository;
 import br.com.chronos.core.modules.collaboration.domain.dtos.CollaboratorDto;
 import br.com.chronos.core.modules.collaboration.interfaces.repositories.CollaboratorsRepository;
 import br.com.chronos.core.modules.collaboration.use_cases.CreateCollaboratorUseCase;
+import br.com.chronos.core.modules.global.domain.records.Id;
+import br.com.chronos.core.modules.auth.domain.records.Password;
 import br.com.chronos.core.modules.global.interfaces.providers.AuthenticationProvider;
+import lombok.Data;
 
 @CollaboratorsController
 public class CreateCollaboratorController {
 
   @Autowired
-  private CollaboratorsRepository repository;
+  private CollaboratorsRepository collaboratorsRepository;
 
   @Autowired
   private AuthenticationProvider authenticationProvider;
 
+  @Autowired
+  private AccountsRepository accountsRepository;
+
+  @Data
+  private static class Request {
+    CollaboratorDto collaboratorDto;
+    Id workScheduleId;
+    String password;
+  }
+
   @PostMapping
-  public ResponseEntity<CollaboratorDto> handle(@RequestBody CollaboratorDto body) {
-    var useCase = new CreateCollaboratorUseCase(repository, authenticationProvider);
-    var responsibleEmail = authenticationProvider.getAuthenticatedUser().getEmail();
-    var collaboratorDto = useCase.execute(body, responsibleEmail);
+  public ResponseEntity<CollaboratorDto> handle(@RequestBody Request body) {
+    System.out.println(body);
+    var useCase = new CreateCollaboratorUseCase(accountsRepository, collaboratorsRepository, authenticationProvider);
+    var password = Password.create(body.password);
+    var responsible = authenticationProvider.getAuthenticatedUser().getEmail();
+    var collaboratorDto = useCase.execute(body.collaboratorDto, body.workScheduleId, password, responsible);
     return ResponseEntity.status(HttpStatus.OK).body(collaboratorDto);
   }
 }
