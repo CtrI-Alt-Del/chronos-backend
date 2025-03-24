cef50ff3b8ac319ae0641d5dec1bc1cdab5e5f57
JohnPetros <joaopedro.nc@outlook.com>
2025-03-24 06:35:19 -0300

    🐛 fix: path variable of EditTimePunchScheduleController

--- src/main/java/br/com/chronos/server/api/controllers/work_schedule/time_punches/EditTimePunchScheduleController.java
+++ src/main/java/br/com/chronos/server/api/controllers/work_schedule/time_punches/EditTimePunchScheduleController.java
@@ -1,16 +1,14 @@
 package br.com.chronos.server.api.controllers.work_schedule.time_punches;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import br.com.chronos.core.modules.work_schedule.domain.dtos.TimePunchDto;
 import br.com.chronos.core.modules.work_schedule.interfaces.repositories.TimePunchesRepository;
 import br.com.chronos.core.modules.work_schedule.use_cases.EditTimePunchScheduleUseCase;
-
-import org.springframework.web.bind.annotation.PutMapping;
 
 @TimePunchesController
 public class EditTimePunchScheduleController {
@@ -18,12 +16,11 @@ public class EditTimePunchScheduleController {
   @Autowired
   private TimePunchesRepository timePunchesRepository;
 
-
   @PutMapping("/{timePunchId}")
-  public ResponseEntity<TimePunchDto> handle(@RequestParam String timePunchId, @RequestBody TimePunchDto body) {
+  public ResponseEntity<TimePunchDto> handle(@PathVariable String timePunchId, @RequestBody TimePunchDto body) {
     body.setId(timePunchId);
     var useCase = new EditTimePunchScheduleUseCase(timePunchesRepository);
     var timePunchDto = useCase.execute(body);
-    return ResponseEntity.status(HttpStatus.OK).body(timePunchDto);
+    return ResponseEntity.ok(timePunchDto);
   }
 }
