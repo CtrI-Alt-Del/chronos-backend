5c4811096b222922b3766b53e9830dc051fd9b09
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-03-25 10:46:15 -0300

    🌐 domain: add Aggregate abstract

---
+++ src/main/java/br/com/chronos/core/modules/global/domain/abstracts/Aggregate.java
@@ -0,0 +1,25 @@
+package br.com.chronos.core.modules.global.domain.abstracts;
+
+import br.com.chronos.core.modules.global.domain.exceptions.NullEntityException;
+import br.com.chronos.core.modules.global.domain.records.Logical;
+
+public class Aggregate<AggregableEntity> extends Entity {
+  private AggregableEntity entity;
+  private String entityName;
+
+  public Aggregate(String id, AggregableEntity entity, String entityName) {
+    super(id);
+    this.entity = entity;
+  }
+
+  public AggregableEntity getEntity() {
+    if (isEntityNull().isTrue()) {
+      throw new NullEntityException(entityName);
+    }
+    return entity;
+  }
+
+  public Logical isEntityNull() {
+    return Logical.create(entity == null);
+  }
+}
