7851596875a7657905f2da5a566cce14c2da8dd8
kaufon <kauanfvale@gmail.com>
2025-03-25 18:55:30 -0300

    🚑hotfix: add cascadetype import to workschedule model

--- src/main/java/br/com/chronos/server/database/jpa/work_schedule/models/WorkScheduleModel.java
+++ src/main/java/br/com/chronos/server/database/jpa/work_schedule/models/WorkScheduleModel.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 
 import br.com.chronos.server.database.jpa.collaborator.models.CollaboratorModel;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
