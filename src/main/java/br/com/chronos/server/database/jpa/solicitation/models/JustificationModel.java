42ea1802f8350694a55179c4f359eaa7fc1d2593
kaufon <kauanfvale@gmail.com>
2025-03-26 14:41:27 -0300

    ♻️refactor: comment everything in justification model

--- src/main/java/br/com/chronos/server/database/jpa/solicitation/models/JustificationModel.java
+++ src/main/java/br/com/chronos/server/database/jpa/solicitation/models/JustificationModel.java
@@ -1,32 +1,32 @@
-package br.com.chronos.server.database.jpa.solicitation.models;
-
-import java.util.UUID;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table(name = "justifications")
-public class JustificationModel {
-
-  @Id
-  private UUID id;
-
-  @Column(nullable = true)
-  private String description;
-
-  @OneToOne(mappedBy = "justification")
-  private WorkdayLogAdjustmentSolicitationModel workdayLogAdjustmentSolicitation;
-
-}
+// package br.com.chronos.server.database.jpa.solicitation.models;
+//
+// import java.util.UUID;
+//
+// import jakarta.persistence.Column;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.OneToOne;
+// import jakarta.persistence.Table;
+// import lombok.AllArgsConstructor;
+// import lombok.Builder;
+// import lombok.Data;
+// import lombok.NoArgsConstructor;
+//
+// @Data
+// @Builder
+// @NoArgsConstructor
+// @AllArgsConstructor
+// @Entity
+// @Table(name = "justifications")
+// public class JustificationModel {
+//
+//   @Id
+//   private UUID id;
+//
+//   @Column(nullable = true)
+//   private String description;
+//
+//   @OneToOne(mappedBy = "justification")
+//   private WorkdayLogAdjustmentSolicitationModel workdayLogAdjustmentSolicitation;
+//
+// }
