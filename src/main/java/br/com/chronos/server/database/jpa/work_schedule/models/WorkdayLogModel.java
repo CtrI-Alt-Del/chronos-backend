33ce8c0bacac307ca2b2701f7403accf6d272bbf
kaufon <kauanfvale@gmail.com>
2025-03-26 14:12:32 -0300

    💾database: add workDayLogAdjustmentSolication to workDayLog

--- src/main/java/br/com/chronos/server/database/jpa/work_schedule/models/WorkdayLogModel.java
+++ src/main/java/br/com/chronos/server/database/jpa/work_schedule/models/WorkdayLogModel.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -19,6 +20,7 @@ import lombok.NoArgsConstructor;
 
 import br.com.chronos.core.modules.work_schedule.domain.records.WorkdayStatus.WorkdayStatusName;
 import br.com.chronos.server.database.jpa.collaborator.models.CollaboratorModel;
+import br.com.chronos.server.database.jpa.solicitation.models.WorkdayLogAdjustmentSolicitationModel;
 
 @Entity
 @AllArgsConstructor
@@ -27,25 +29,28 @@ import br.com.chronos.server.database.jpa.collaborator.models.CollaboratorModel;
 @Data
 @Table(name = "workday_logs")
 public class WorkdayLogModel {
-    @Id
-    private UUID id;
+  @Id
+  private UUID id;
 
-    @Column(name = "date", nullable = false)
-    private LocalDate date;
+  @Column(name = "date", nullable = false)
+  private LocalDate date;
 
-    @ManyToOne
-    @JoinColumn(name = "collaborator_id", nullable = false)
-    private CollaboratorModel collaborator;
+  @ManyToOne
+  @JoinColumn(name = "collaborator_id", nullable = false)
+  private CollaboratorModel collaborator;
 
-    @ManyToOne
-    @JoinColumn(name = "time_punch_schedule_id", nullable = false)
-    private TimePunchModel timePunchSchedule;
+  @ManyToOne
+  @JoinColumn(name = "time_punch_schedule_id", nullable = false)
+  private TimePunchModel timePunchSchedule;
 
-    @OneToOne
-    @JoinColumn(name = "time_punch_log_id", nullable = false)
-    private TimePunchModel timePunchLog;
+  @OneToOne
+  @JoinColumn(name = "time_punch_log_id", nullable = false)
+  private TimePunchModel timePunchLog;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "status", nullable = false)
-    private WorkdayStatusName status;
+  @Enumerated(EnumType.STRING)
+  @Column(name = "status", nullable = false)
+  private WorkdayStatusName status;
+
+  @OneToMany(mappedBy = "workdayLog")
+  private WorkdayLogAdjustmentSolicitationModel workdayLogAdjustmentSolicitation;
 }
