1c3622d9df49d3c94bd66909ed761f9cabe06650
kaufon <kauanfvale@gmail.com>
2025-03-27 23:04:43 -0300

    📑interface: add new method SolicitationRepository

--- src/main/java/br/com/chronos/core/modules/solicitation/interfaces/repository/SolicitationsRepository.java
+++ src/main/java/br/com/chronos/core/modules/solicitation/interfaces/repository/SolicitationsRepository.java
@@ -1,8 +1,10 @@
 package br.com.chronos.core.modules.solicitation.interfaces.repository;
 
 import br.com.chronos.core.modules.solicitation.domain.entities.TimePunchLogAdjustmentSolicitation;
+import br.com.chronos.core.modules.solicitation.domain.entities.WorkScheduleAdjustmentSolicitation;
 
 public interface SolicitationsRepository {
     void addTimePunchLogAdjustmentSolicitation(TimePunchLogAdjustmentSolicitation solicitation);
+    void addWorkScheduleAdjustmentSolicitation(WorkScheduleAdjustmentSolicitation solicitation);
     
 }
