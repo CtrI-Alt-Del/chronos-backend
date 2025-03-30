d86bed0a2f7000ebd3d55a1bda802aec70edc3a9
kaufon <kauanfvale@gmail.com>
2025-03-29 21:15:07 -0300

    ♻️refacotr: create workSchedule adjustment use case

--- src/main/java/br/com/chronos/core/modules/solicitation/use_cases/CreateTimePunchLogAdjustmentSolicitationUseCase.java
+++ src/main/java/br/com/chronos/core/modules/solicitation/use_cases/CreateTimePunchLogAdjustmentSolicitationUseCase.java
@@ -1,22 +1,43 @@
 package br.com.chronos.core.modules.solicitation.use_cases;
 
+import br.com.chronos.core.modules.auth.domain.entities.Account;
+import br.com.chronos.core.modules.collaboration.interfaces.repositories.CollaboratorsRepository;
+import br.com.chronos.core.modules.global.domain.dtos.ResponsibleAggregateDto;
+import br.com.chronos.core.modules.global.domain.dtos.ResponsibleDto;
+import br.com.chronos.core.modules.global.domain.exceptions.NotFoundException;
+import br.com.chronos.core.modules.global.domain.records.Id;
 import br.com.chronos.core.modules.solicitation.domain.dtos.TimePunchLogAdjustmentSolicitationDto;
 import br.com.chronos.core.modules.solicitation.domain.entities.TimePunchLogAdjustmentSolicitation;
 import br.com.chronos.core.modules.solicitation.interfaces.repository.SolicitationsRepository;
 
 public class CreateTimePunchLogAdjustmentSolicitationUseCase {
 
-    private final SolicitationsRepository repository;
+  private final SolicitationsRepository solicitationsRepository;
+  private final CollaboratorsRepository collaboratorsRepository;
 
-    public CreateTimePunchLogAdjustmentSolicitationUseCase(SolicitationsRepository repository) {
-        this.repository = repository;
+  public CreateTimePunchLogAdjustmentSolicitationUseCase(SolicitationsRepository solicitationsRepository,CollaboratorsRepository collaboratorsRepository) {
+    this.collaboratorsRepository = collaboratorsRepository;
+    this.solicitationsRepository = solicitationsRepository;
+  }
+
+  public TimePunchLogAdjustmentSolicitationDto execute(TimePunchLogAdjustmentSolicitationDto dto, Account sender) {
+    var senderResponsibleDto = new ResponsibleDto()
+        .setId(sender.getCollaboratorId().value().toString())
+        .setEmail(sender.getEmail().value().toString())
+        .setRole(sender.getRole().value().toString())
+        .setName(findSenderName(sender.getCollaboratorId()));
+    dto.setSenderResponsible(new ResponsibleAggregateDto(senderResponsibleDto));
+    var solicitation = new TimePunchLogAdjustmentSolicitation(dto);
+    solicitationsRepository.addTimePunchLogAdjustmentSolicitation(solicitation);
+    return solicitation.getDto();
+  }
+
+  private String findSenderName(Id collaboratorId) {
+    var collaborator = collaboratorsRepository.findById(collaboratorId);
+    if (collaborator.isEmpty()) {
+      throw new NotFoundException("Collaborator not found");
     }
-
-    public TimePunchLogAdjustmentSolicitationDto execute(TimePunchLogAdjustmentSolicitationDto dto) {
-        var solicitation = new TimePunchLogAdjustmentSolicitation(dto);
-        repository.addTimePunchLogAdjustmentSolicitation(solicitation);
-        return solicitation.getDto();
-    }
-
+    return collaborator.get().getName().value().toString();
+  }
 
 }
