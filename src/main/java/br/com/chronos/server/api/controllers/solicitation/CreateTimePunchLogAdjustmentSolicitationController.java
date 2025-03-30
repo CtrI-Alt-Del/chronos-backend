a04b50faa4cd165bde27fb039cde4f71de42560d
kaufon <kauanfvale@gmail.com>
2025-03-29 21:14:48 -0300

    ♻️refactor: create time punch adjustment solicitation controller

--- src/main/java/br/com/chronos/server/api/controllers/solicitation/CreateTimePunchLogAdjustmentSolicitationController.java
+++ src/main/java/br/com/chronos/server/api/controllers/solicitation/CreateTimePunchLogAdjustmentSolicitationController.java
@@ -2,9 +2,12 @@ package br.com.chronos.server.api.controllers.solicitation;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import br.com.chronos.core.modules.collaboration.interfaces.repositories.CollaboratorsRepository;
+import br.com.chronos.core.modules.global.interfaces.providers.AuthenticationProvider;
 import br.com.chronos.core.modules.solicitation.domain.dtos.TimePunchLogAdjustmentSolicitationDto;
 import br.com.chronos.core.modules.solicitation.interfaces.repository.SolicitationsRepository;
 import br.com.chronos.core.modules.solicitation.use_cases.CreateTimePunchLogAdjustmentSolicitationUseCase;
@@ -14,12 +17,18 @@ public class CreateTimePunchLogAdjustmentSolicitationController {
 
   @Autowired
   private SolicitationsRepository solicitationsRepository;
+  @Autowired
+  private CollaboratorsRepository collaboratorsRepository;
 
-    @PostMapping("/time-punch-adjustment")
-    public ResponseEntity<TimePunchLogAdjustmentSolicitationDto> handle(
-            @RequestBody TimePunchLogAdjustmentSolicitationDto body) {
-        var useCase = new CreateTimePunchLogAdjustmentSolicitationUseCase(solicitationsRepository);
-        var timePunchLogAdjustmentSolicitationDto = useCase.execute(body);
-        return ResponseEntity.ok(timePunchLogAdjustmentSolicitationDto);
-    }
+  @Autowired
+  private AuthenticationProvider authenticationProvider;
+
+  @PostMapping("/time-punch-adjustment")
+  public ResponseEntity<TimePunchLogAdjustmentSolicitationDto> handle(
+      @RequestBody TimePunchLogAdjustmentSolicitationDto body) {
+    var useCase = new CreateTimePunchLogAdjustmentSolicitationUseCase(solicitationsRepository,collaboratorsRepository);
+    var responsible = authenticationProvider.getAuthenticatedUser();
+    var timePunchLogAdjustmentSolicitationDto = useCase.execute(body,responsible);
+    return ResponseEntity.ok(timePunchLogAdjustmentSolicitationDto);
+  }
 }
