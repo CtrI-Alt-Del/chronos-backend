d36ea48ffad9443a5af60e754fd22aedaa9d725e
kaufon <kauanfvale@gmail.com>
2025-03-31 21:02:54 -0300

    ♻️refactor: create time punch adjustment use case

--- src/main/java/br/com/chronos/core/modules/solicitation/use_cases/CreateTimePunchLogAdjustmentSolicitationUseCase.java
+++ src/main/java/br/com/chronos/core/modules/solicitation/use_cases/CreateTimePunchLogAdjustmentSolicitationUseCase.java
@@ -1,33 +1,41 @@
 package br.com.chronos.core.modules.solicitation.use_cases;
 
-import br.com.chronos.core.modules.auth.domain.entities.Account;
 import br.com.chronos.core.modules.collaboration.interfaces.repositories.CollaboratorsRepository;
 import br.com.chronos.core.modules.global.domain.dtos.ResponsibleAggregateDto;
 import br.com.chronos.core.modules.global.domain.dtos.ResponsibleDto;
 import br.com.chronos.core.modules.global.domain.exceptions.NotFoundException;
+import br.com.chronos.core.modules.global.domain.records.Email;
 import br.com.chronos.core.modules.global.domain.records.Id;
+import br.com.chronos.core.modules.global.domain.records.Role;
 import br.com.chronos.core.modules.solicitation.domain.dtos.TimePunchLogAdjustmentSolicitationDto;
 import br.com.chronos.core.modules.solicitation.domain.entities.TimePunchLogAdjustmentSolicitation;
 import br.com.chronos.core.modules.solicitation.interfaces.repository.SolicitationsRepository;
+import br.com.chronos.core.modules.solicitation.domain.records.SolicitationStatus.Status;
+import br.com.chronos.core.modules.global.domain.exceptions.ValidationException;
 
 public class CreateTimePunchLogAdjustmentSolicitationUseCase {
 
   private final SolicitationsRepository solicitationsRepository;
   private final CollaboratorsRepository collaboratorsRepository;
 
-  public CreateTimePunchLogAdjustmentSolicitationUseCase(SolicitationsRepository solicitationsRepository,CollaboratorsRepository collaboratorsRepository) {
+  public CreateTimePunchLogAdjustmentSolicitationUseCase(SolicitationsRepository solicitationsRepository,
+      CollaboratorsRepository collaboratorsRepository) {
     this.collaboratorsRepository = collaboratorsRepository;
     this.solicitationsRepository = solicitationsRepository;
   }
 
-  public TimePunchLogAdjustmentSolicitationDto execute(TimePunchLogAdjustmentSolicitationDto dto, Account sender) {
+  public TimePunchLogAdjustmentSolicitationDto execute(TimePunchLogAdjustmentSolicitationDto dto, Id collaboratorId,
+      Email senderEmail, Role senderRole) {
     var senderResponsibleDto = new ResponsibleDto()
-        .setId(sender.getCollaboratorId().value().toString())
-        .setEmail(sender.getEmail().value().toString())
-        .setRole(sender.getRole().value().toString())
-        .setName(findSenderName(sender.getCollaboratorId()));
+        .setId(collaboratorId.value().toString())
+        .setEmail(senderEmail.value().toString())
+        .setRole(senderRole.value().toString())
+        .setName(findSenderName(collaboratorId));
     dto.setSenderResponsible(new ResponsibleAggregateDto(senderResponsibleDto));
     var solicitation = new TimePunchLogAdjustmentSolicitation(dto);
+    if (solicitation.status.value() != Status.PENDING) {
+      throw new ValidationException("status", "deve ser pendente");
+    }
     solicitationsRepository.addTimePunchLogAdjustmentSolicitation(solicitation);
     return solicitation.getDto();
   }
