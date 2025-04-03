b79bffdf9f50448b7ad9c4499e99ad7fa26ee2ae
kaufon <kauanfvale@gmail.com>
2025-04-02 22:14:53 -0300

    ♻️refactor: jpa solicitation repository

--- src/main/java/br/com/chronos/server/database/jpa/solicitation/repositories/JpaSolicitationsRepository.java
+++ src/main/java/br/com/chronos/server/database/jpa/solicitation/repositories/JpaSolicitationsRepository.java
@@ -3,10 +3,8 @@ package br.com.chronos.server.database.jpa.solicitation.repositories;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.chronos.core.modules.global.domain.records.Array;
@@ -14,92 +12,44 @@ import br.com.chronos.core.modules.global.domain.records.CollaborationSector.Sec
 import br.com.chronos.core.modules.global.domain.records.Id;
 import br.com.chronos.core.modules.global.domain.records.Role;
 import br.com.chronos.core.modules.solicitation.domain.abstracts.Solicitation;
+import br.com.chronos.core.modules.solicitation.domain.entities.DayOffScheduleAdjustmentSolicitation;
 import br.com.chronos.core.modules.solicitation.domain.entities.TimePunchLogAdjustmentSolicitation;
-import br.com.chronos.core.modules.solicitation.domain.entities.WorkScheduleAdjustmentSolicitation;
+import br.com.chronos.core.modules.solicitation.interfaces.repository.DayOffScheduleAdjustmentRepository;
 import br.com.chronos.core.modules.solicitation.interfaces.repository.SolicitationsRepository;
-import br.com.chronos.server.database.jpa.solicitation.mappers.TimePunchLogAdjustmentSolicitationMapper;
-import br.com.chronos.server.database.jpa.solicitation.mappers.WorkScheduleAdjustmentSolicitationMapper;
-import br.com.chronos.server.database.jpa.solicitation.models.TimePunchLogAdjustmentSolicitationModel;
-import br.com.chronos.server.database.jpa.solicitation.models.WorkScheduleAdjustmentSolicitationModel;
-
-interface JpaTimePunchLogAdjustmentSolicitationModelsRepository
-    extends JpaRepository<TimePunchLogAdjustmentSolicitationModel, UUID> {
-  List<TimePunchLogAdjustmentSolicitationModel> findAllBySenderResponsibleAccountSector(Sector sector);
-
-  List<TimePunchLogAdjustmentSolicitationModel> findAllBySenderResponsibleId(
-      UUID userId);
-}
-
-interface JpaWorkScheduleAdjustmentSolicitationModelsRepository
-    extends JpaRepository<WorkScheduleAdjustmentSolicitationModel, UUID> {
-  List<WorkScheduleAdjustmentSolicitationModel> findAllBySenderResponsibleAccountSector(Sector sector);
-
-  List<WorkScheduleAdjustmentSolicitationModel> findAllBySenderResponsibleId(
-      UUID userId);
-}
+import br.com.chronos.core.modules.solicitation.interfaces.repository.TimePunchLogAdjustmentRepository;
 
 public class JpaSolicitationsRepository implements SolicitationsRepository {
 
   @Autowired
-  JpaTimePunchLogAdjustmentSolicitationModelsRepository timePunchLogAdjustmentSolicitationModelsRepository;
+  TimePunchLogAdjustmentRepository timePunchLogAdjustmentSolicitationModelsRepository;
 
   @Autowired
-  JpaWorkScheduleAdjustmentSolicitationModelsRepository workScheduleAdjustmentSolcitationModelsRepository;
-
-  @Autowired
-  TimePunchLogAdjustmentSolicitationMapper timePunchAdjustmentMapper;
-
-  @Autowired
-  WorkScheduleAdjustmentSolicitationMapper workScheduleAdjustmentMapper;
-
-  @Override
-  @Transactional
-  public void addTimePunchLogAdjustmentSolicitation(TimePunchLogAdjustmentSolicitation solicitation) {
-    var timePunchAdjustmentSolicitationModel = timePunchAdjustmentMapper.toModel(solicitation);
-    timePunchLogAdjustmentSolicitationModelsRepository.save(timePunchAdjustmentSolicitationModel);
-  }
-
-  @Override
-  @Transactional
-  public void addWorkScheduleAdjustmentSolicitation(WorkScheduleAdjustmentSolicitation solicitation) {
-    var workScheduleAdjustmentSolicitationModel = workScheduleAdjustmentMapper.toModel(solicitation);
-    workScheduleAdjustmentSolcitationModelsRepository.save(workScheduleAdjustmentSolicitationModel);
-  }
+  DayOffScheduleAdjustmentRepository dayOffScheduleAdjustmentSolcitationModelsRepository;
 
   @Override
   @Transactional(readOnly = true)
   public Array<Solicitation> findMany(Sector sector, Role role, Id userId) {
-    List<TimePunchLogAdjustmentSolicitationModel> timePunchSolicitations;
-    List<WorkScheduleAdjustmentSolicitationModel> workScheduleSolicitations;
+    List<TimePunchLogAdjustmentSolicitation> timePunchSolicitations;
+    List<DayOffScheduleAdjustmentSolicitation> dayOffScheduleSolicitations;
 
     if (role.isEmployee().isTrue()) {
       timePunchSolicitations = timePunchLogAdjustmentSolicitationModelsRepository
-          .findAllBySenderResponsibleId(userId.value());
+          .findAllByCollaboratorId(userId).list();
 
-      workScheduleSolicitations = workScheduleAdjustmentSolcitationModelsRepository
-          .findAllBySenderResponsibleId(userId.value());
+      dayOffScheduleSolicitations = dayOffScheduleAdjustmentSolcitationModelsRepository
+          .findAllByCollaboratorId(userId).list();
     } else {
-      
-      timePunchSolicitations = timePunchLogAdjustmentSolicitationModelsRepository
-          .findAllBySenderResponsibleAccountSector(sector);
 
-      workScheduleSolicitations = workScheduleAdjustmentSolcitationModelsRepository
-          .findAllBySenderResponsibleAccountSector(sector);
+      timePunchSolicitations = timePunchLogAdjustmentSolicitationModelsRepository
+          .findAllByCollaboratorSector(sector).list();
+
+      dayOffScheduleSolicitations = dayOffScheduleAdjustmentSolcitationModelsRepository
+          .findAllByCollaboratorSector(sector).list();
     }
 
-    
-    var timePunchSolicitationsEntities = timePunchSolicitations.stream()
-        .map(timePunchAdjustmentMapper::toEntity)
-        .toList();
-
-    var workScheduleSolicitationsEntities = workScheduleSolicitations.stream()
-        .map(workScheduleAdjustmentMapper::toEntity)
-        .toList();
-
-    
     var allSolicitations = new ArrayList<Solicitation>();
-    allSolicitations.addAll(timePunchSolicitationsEntities);
-    allSolicitations.addAll(workScheduleSolicitationsEntities);
+    allSolicitations.addAll(timePunchSolicitations);
+    allSolicitations.addAll(dayOffScheduleSolicitations);
 
     return new Array<>(allSolicitations);
   }
@@ -109,38 +59,27 @@ public class JpaSolicitationsRepository implements SolicitationsRepository {
     if (solicitation instanceof TimePunchLogAdjustmentSolicitation) {
       TimePunchLogAdjustmentSolicitation timePunchSolicitation = (TimePunchLogAdjustmentSolicitation) solicitation;
 
-      timePunchSolicitation.status = solicitation.status;
+      timePunchLogAdjustmentSolicitationModelsRepository.resolveSolicitation(timePunchSolicitation);
 
-      var timePunchSolicitationModel = timePunchAdjustmentMapper.toModel(timePunchSolicitation);
-      timePunchLogAdjustmentSolicitationModelsRepository.save(timePunchSolicitationModel);
-
-    } else if (solicitation instanceof WorkScheduleAdjustmentSolicitation) {
-      WorkScheduleAdjustmentSolicitation workScheduleSolicitation = (WorkScheduleAdjustmentSolicitation) solicitation;
-
-      workScheduleSolicitation.status = solicitation.status;
-
-      var workScheduleSolicitationModel = workScheduleAdjustmentMapper.toModel(workScheduleSolicitation);
-      workScheduleAdjustmentSolcitationModelsRepository.save(workScheduleSolicitationModel);
+    } else if (solicitation instanceof DayOffScheduleAdjustmentSolicitation) {
+      DayOffScheduleAdjustmentSolicitation dayOffScheduleSolicitation = (DayOffScheduleAdjustmentSolicitation) solicitation;
+      dayOffScheduleAdjustmentSolcitationModelsRepository.resolveSolicitation(dayOffScheduleSolicitation);
     }
   }
 
   @Override
   public Optional<Solicitation> findSolicitationById(Id solicitationId) {
-    var timePunchSolicitationModel = timePunchLogAdjustmentSolicitationModelsRepository
-        .findById(solicitationId.value());
-
-    if (timePunchSolicitationModel.isPresent()) {
-      return Optional.of(timePunchAdjustmentMapper.toEntity(timePunchSolicitationModel.get()));
+    var timePunchSolicitation = timePunchLogAdjustmentSolicitationModelsRepository.findSolicitationById(solicitationId);
+    if (timePunchSolicitation.isPresent()) {
+      return Optional.of(timePunchSolicitation.get());
     }
 
-    var workScheduleSolicitationModel = workScheduleAdjustmentSolcitationModelsRepository
-        .findById(solicitationId.value());
-
-    if (workScheduleSolicitationModel.isPresent()) {
-      return Optional.of(workScheduleAdjustmentMapper.toEntity(workScheduleSolicitationModel.get()));
+    var dayOffScheduleSolicitation = dayOffScheduleAdjustmentSolcitationModelsRepository
+        .findSolicitationById(solicitationId);
+    if (dayOffScheduleSolicitation.isPresent()) {
+      return Optional.of(dayOffScheduleSolicitation.get());
     }
+
     return Optional.empty();
-
   }
-
 }
