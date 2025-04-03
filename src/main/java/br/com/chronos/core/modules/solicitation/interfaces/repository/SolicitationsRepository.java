74997f34c8ff1826a25b8c2627df5780015cc63b
kaufon <kauanfvale@gmail.com>
2025-04-02 22:10:51 -0300

    ♻️refactor: solicitation repository interface

--- src/main/java/br/com/chronos/core/modules/solicitation/interfaces/repository/SolicitationsRepository.java
+++ src/main/java/br/com/chronos/core/modules/solicitation/interfaces/repository/SolicitationsRepository.java
@@ -1,21 +1,16 @@
 package br.com.chronos.core.modules.solicitation.interfaces.repository;
 
 
-import br.com.chronos.core.modules.global.domain.records.CollaborationSector.Sector;
-import br.com.chronos.core.modules.solicitation.domain.abstracts.Solicitation;
-import br.com.chronos.core.modules.solicitation.domain.entities.TimePunchLogAdjustmentSolicitation;
-import br.com.chronos.core.modules.solicitation.domain.entities.WorkScheduleAdjustmentSolicitation;
-
 import java.util.Optional;
 
 import br.com.chronos.core.modules.global.domain.records.Array;
+import br.com.chronos.core.modules.global.domain.records.CollaborationSector.Sector;
 import br.com.chronos.core.modules.global.domain.records.Id;
 import br.com.chronos.core.modules.global.domain.records.Role;
+import br.com.chronos.core.modules.solicitation.domain.abstracts.Solicitation;
 
 public interface SolicitationsRepository {
-  void addTimePunchLogAdjustmentSolicitation(TimePunchLogAdjustmentSolicitation solicitation);
 
-  void addWorkScheduleAdjustmentSolicitation(WorkScheduleAdjustmentSolicitation solicitation);
 
   Array<Solicitation> findMany(Sector sector,Role role,Id userId);
 
