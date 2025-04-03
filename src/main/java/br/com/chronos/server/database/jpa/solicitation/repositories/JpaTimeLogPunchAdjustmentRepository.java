a671b283e40f09aa03862e8cd9bd9b9fa99b26d0
kaufon <kauanfvale@gmail.com>
2025-04-02 22:17:24 -0300

    💾database: add jpa time punch log adjustment repository

---
+++ src/main/java/br/com/chronos/server/database/jpa/solicitation/repositories/JpaTimeLogPunchAdjustmentRepository.java
@@ -0,0 +1,63 @@
+package br.com.chronos.server.database.jpa.solicitation.repositories;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.chronos.core.modules.global.domain.records.Array;
+import br.com.chronos.core.modules.global.domain.records.CollaborationSector.Sector;
+import br.com.chronos.core.modules.global.domain.records.Id;
+import br.com.chronos.core.modules.solicitation.domain.entities.TimePunchLogAdjustmentSolicitation;
+import br.com.chronos.core.modules.solicitation.interfaces.repository.TimePunchLogAdjustmentRepository;
+import br.com.chronos.server.database.jpa.solicitation.mappers.TimePunchLogAdjustmentSolicitationMapper;
+import br.com.chronos.server.database.jpa.solicitation.models.TimePunchLogAdjustmentSolicitationModel;
+
+interface JpaTimePunchAdjustmentSolicitationRepository
+    extends JpaRepository<TimePunchLogAdjustmentSolicitationModel, UUID> {
+  List<TimePunchLogAdjustmentSolicitationModel> findAllBySenderResponsibleAccountSector(Sector sector);
+
+  List<TimePunchLogAdjustmentSolicitationModel> findAllBySenderResponsibleId(
+      UUID userId);
+}
+
+public class JpaTimeLogPunchAdjustmentRepository implements TimePunchLogAdjustmentRepository {
+
+  @Autowired
+  private JpaTimePunchAdjustmentSolicitationRepository solicitationRepository;
+
+  @Autowired
+  private TimePunchLogAdjustmentSolicitationMapper mapper;
+
+  @Override
+  public void add(TimePunchLogAdjustmentSolicitation solicitation) {
+    var solicitationModel = mapper.toModel(solicitation);
+    solicitationRepository.save(solicitationModel);
+  }
+
+  @Override
+  public void resolveSolicitation(TimePunchLogAdjustmentSolicitation solicitation) {
+    var solicitationModel = mapper.toModel(solicitation);
+    solicitationRepository.save(solicitationModel);
+  }
+
+  @Override
+  public Optional<TimePunchLogAdjustmentSolicitation> findSolicitationById(Id solicitationId) {
+    return solicitationRepository.findById(solicitationId.value())
+        .map(mapper::toEntity);
+  }
+
+  @Override
+  public Array<TimePunchLogAdjustmentSolicitation> findAllByCollaboratorId(Id userId) {
+    var solicitationModels = solicitationRepository.findAllBySenderResponsibleId(userId.value());
+    return Array.createFrom(solicitationModels, mapper::toEntity);
+  }
+
+  @Override
+  public Array<TimePunchLogAdjustmentSolicitation> findAllByCollaboratorSector(Sector sector) {
+    var solicitationModels = solicitationRepository.findAllBySenderResponsibleAccountSector(sector);
+    return Array.createFrom(solicitationModels, mapper::toEntity);
+  }
+}
