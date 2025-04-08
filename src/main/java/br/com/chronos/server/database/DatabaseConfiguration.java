f3b9bded71204a13c54f4029e92e12d57f149029
eduardokuwaharajr57@gmail.com <eduardokuwaharajr57@gmail.com>
2025-04-08 18:34:21 -0300

    ♻ refactos: adding repository imports

--- src/main/java/br/com/chronos/server/database/DatabaseConfiguration.java
+++ src/main/java/br/com/chronos/server/database/DatabaseConfiguration.java
@@ -7,11 +7,12 @@ import br.com.chronos.core.modules.auth.interfaces.repositories.AccountsReposito
 import br.com.chronos.core.modules.collaboration.interfaces.repositories.CollaboratorsRepository;
 import br.com.chronos.core.modules.solicitation.interfaces.repository.DayOffScheduleAdjustmentRepository;
 import br.com.chronos.core.modules.solicitation.interfaces.repository.SolicitationsRepository;
-
+import br.com.chronos.core.modules.solicitation.interfaces.repository.TimePunchLogAdjustmentRepository;
 import br.com.chronos.core.modules.work_schedule.interfaces.repositories.TimePunchesRepository;
 import br.com.chronos.core.modules.work_schedule.interfaces.repositories.WeekdaySchedulesRepository;
 import br.com.chronos.core.modules.work_schedule.interfaces.repositories.WorkSchedulesRepository;
 import br.com.chronos.core.modules.work_schedule.interfaces.repositories.WorkdayLogsRepository;
+import br.com.chronos.core.modules.work_schedule.interfaces.repositories.DayOffSchedulesRepository;
 import br.com.chronos.server.database.jpa.auth.repositories.JpaAccountsRepository;
 import br.com.chronos.server.database.jpa.collaborator.repositories.JpaCollaboratorsRepository;
 import br.com.chronos.server.database.jpa.solicitation.repositories.JpaDayOffScheduleAdjustmentSolicitationsRepository;
@@ -21,6 +22,7 @@ import br.com.chronos.server.database.jpa.work_schedule.repositories.JpaTimePunc
 import br.com.chronos.server.database.jpa.work_schedule.repositories.JpaWeekdaySchedulesRepository;
 import br.com.chronos.server.database.jpa.work_schedule.repositories.JpaWorkSchedulesRepository;
 import br.com.chronos.server.database.jpa.work_schedule.repositories.JpaWorkdayLogsRepository;
+import br.com.chronos.server.database.jpa.work_schedule.repositories.JpaDayOffSchedulesRepository;
 
 @Configuration
 public class DatabaseConfiguration {
