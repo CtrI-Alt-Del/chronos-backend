7d44e407b9e342f8882e72efca44442aba0f9f9e
JohnPetros <joaopedro.nc@outlook.com>
2025-04-10 22:59:00 -0300

    ⚙️ config: expose Jackson2JsonMessageConverter for rabbitmq messages convertion to json

--- src/main/java/br/com/chronos/server/queue/rabbitmq/RabbitMqConfiguration.java
+++ src/main/java/br/com/chronos/server/queue/rabbitmq/RabbitMqConfiguration.java
@@ -3,12 +3,19 @@ package br.com.chronos.server.queue.rabbitmq;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+
+import br.com.chronos.core.work_schedule.domain.events.TimePunchClosedEvent;
 
 @Configuration
 public class RabbitMqConfiguration {
+  @Bean
+  Queue timePunchClosedEventQueue() {
+    return new Queue(TimePunchClosedEvent.KEY, true);
+  }
 
   @Bean
-  Queue queue() {
-    return new Queue("solicitation-queue", true);
+  Jackson2JsonMessageConverter messageConverter() {
+    return new Jackson2JsonMessageConverter();
   }
 }
