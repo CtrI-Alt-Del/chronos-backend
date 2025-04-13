98b187f36284195974effa81a93afc1c2a59fa35
eduardokuwaharajr57@gmail.com <eduardokuwaharajr57@gmail.com>
2025-04-13 19:41:29 -0300

    🚧 wip: Entity to model conversion

--- src/main/java/br/com/chronos/server/database/mongodb/hour_bank/mappers/HourBankTransactionMapper.java
+++ src/main/java/br/com/chronos/server/database/mongodb/hour_bank/mappers/HourBankTransactionMapper.java
@@ -1,5 +1,17 @@
 package br.com.chronos.server.database.mongodb.hour_bank.mappers;
 
-public class HourBankTransactionMapper {
+import br.com.chronos.core.hour_bank.domain.dtos.HourBankTransactionDto;
+import br.com.chronos.server.database.mongodb.hour_bank.models.HourBankTransactionModel;
+
+public class HourBankTransactionMapper {   //HourBankTransactionEntity entity
+    public HourBankTransactionModel toModel(HourBankTransaction entity) {
+        var model = new HourBankTransactionModel()
+                .setId(entity.getId())
+                .setOperation(entity.getOperation())
+                .setReason(entity.getReason())
+                .setOperated_at(entity.getOperatedAt());
+
+        return model;
+    }
     
 }
