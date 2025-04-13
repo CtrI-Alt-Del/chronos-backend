859b781c705d1f79eea1fcfd8eddee04a917e3f0
eduardokuwaharajr57@gmail.com <eduardokuwaharajr57@gmail.com>
2025-04-13 19:39:53 -0300

    🚧 wip: Including attributes and lombok notations

--- src/main/java/br/com/chronos/server/database/mongodb/hour_bank/models/HourBankTransactionModel.java
+++ src/main/java/br/com/chronos/server/database/mongodb/hour_bank/models/HourBankTransactionModel.java
@@ -1,5 +1,26 @@
 package br.com.chronos.server.database.mongodb.hour_bank.models;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import br.com.chronos.core.hour_bank.domain.records.HourBankReason;
+import br.com.chronos.core.hour_bank.domain.records.HourBankTransaction;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Document(collection = "hour_bank_transactions")
 public class HourBankTransactionModel {
-    
+    @Id
+    private UUID id;
+    private HourBankTransaction operation;
+    private HourBankReason reason;
+    private LocalDate operated_at;
+    // possívelmente um relacionamenro com hour_bank
 }
