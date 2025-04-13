5afab6e2a831ffdec4cf9c5b5097414592c65286
JohnPetros <joaopedro.nc@outlook.com>
2025-04-13 17:00:59 -0300

    🎞️ queue: add CreateWorkdayLogsJob

---
+++ src/main/java/br/com/chronos/server/queue/jobs/work_schedule/CreateWorkdayLogsJob.java
@@ -0,0 +1,23 @@
+package br.com.chronos.server.queue.jobs.work_schedule;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import br.com.chronos.core.collaboration.domain.events.CollaboratorsPreparedForWorkEvent;
+import br.com.chronos.core.work_schedule.interfaces.repositories.DayOffSchedulesRepository;
+import br.com.chronos.core.work_schedule.interfaces.repositories.WorkdayLogsRepository;
+import br.com.chronos.core.work_schedule.use_cases.CreateWorkdayLogsUseCase;
+
+@Component
+public class CreateWorkdayLogsJob {
+  @Autowired
+  private WorkdayLogsRepository workdayLogsRepository;
+
+  @Autowired
+  private DayOffSchedulesRepository dayOffSchedulesRepository;
+
+  public void handle(CollaboratorsPreparedForWorkEvent.Payload payload) {
+    var useCase = new CreateWorkdayLogsUseCase(workdayLogsRepository, dayOffSchedulesRepository);
+    useCase.execute(payload.collaboratorIds, payload.collaboratorWorkloads, payload.date);
+  }
+}
