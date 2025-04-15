c20e1f32ee63cae6085f97ed98de8860b9d4d2d6
eduardokuwaharajr57@gmail.com <eduardokuwaharajr57@gmail.com>
2025-04-15 11:34:14 -0300

    ♻ refactor: Creating toModel and toRecord convertion

--- src/main/java/br/com/chronos/server/database/mongodb/hour_bank/mappers/HourBankTransactionMapper.java
+++ src/main/java/br/com/chronos/server/database/mongodb/hour_bank/mappers/HourBankTransactionMapper.java
@@ -1,17 +1,33 @@
 package br.com.chronos.server.database.mongodb.hour_bank.mappers;
 
-import br.com.chronos.core.hour_bank.domain.dtos.HourBankTransactionDto;
+
+import br.com.chronos.core.global.domain.records.Date;
+import br.com.chronos.core.global.domain.records.Time;
+import br.com.chronos.core.hour_bank.domain.records.HourBankTransaction;
 import br.com.chronos.server.database.mongodb.hour_bank.models.HourBankTransactionModel;
 
-public class HourBankTransactionMapper {   //HourBankTransactionEntity entity
-    public HourBankTransactionModel toModel(HourBankTransaction entity) {
-        var model = new HourBankTransactionModel()
-                .setId(entity.getId())
-                .setOperation(entity.getOperation())
-                .setReason(entity.getReason())
-                .setOperated_at(entity.getOperatedAt());
 
+public class HourBankTransactionMapper {
+    public HourBankTransactionModel toModel(HourBankTransaction record) {
+      var model = HourBankTransactionModel.builder()
+        .operation(record.operation())
+        .reason(record.reason())
+        .time(record.time().value())
+        .date(record.date().value())
+        .build();
         return model;
+          
     }
+
+    public HourBankTransaction toRecord (HourBankTransactionModel model) {
+        var record = new HourBankTransaction(
+            Time.create(model.getTime()),
+            model.getOperation(),
+            model.getReason(),
+            Date.create(model.getDate())
+        );
+        return record;
     
+    }
+
 }
