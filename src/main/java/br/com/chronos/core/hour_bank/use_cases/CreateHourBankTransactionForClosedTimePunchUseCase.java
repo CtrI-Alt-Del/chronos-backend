f9b765338de1408c1e4ad8c1e80468d08320038f
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-04-16 10:11:54 -0300

    ♻️ refactor: use dto to create hour bank transaction in CreateHourBankTransactionForClosedTimePunchUseCase

--- src/main/java/br/com/chronos/core/hour_bank/use_cases/CreateHourBankTransactionForClosedTimePunchUseCase.java
+++ src/main/java/br/com/chronos/core/hour_bank/use_cases/CreateHourBankTransactionForClosedTimePunchUseCase.java
@@ -1,10 +1,12 @@
 package br.com.chronos.core.hour_bank.use_cases;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 
 import br.com.chronos.core.global.domain.records.Array;
 import br.com.chronos.core.global.domain.records.Id;
 import br.com.chronos.core.global.domain.records.Time;
+import br.com.chronos.core.hour_bank.domain.dtos.HourBankTransactionDto;
 import br.com.chronos.core.hour_bank.domain.records.HourBankTransaction;
 import br.com.chronos.core.hour_bank.interfaces.HourBankTransactionsRepository;
 
@@ -15,18 +17,28 @@ public class CreateHourBankTransactionForClosedTimePunchUseCase {
     this.repository = repository;
   }
 
-  public void execute(LocalTime overtimeValue, LocalTime latetimeValue, String collaboratorId) {
+  public void execute(LocalTime overtimeValue, LocalTime latetimeValue, LocalDate date, String collaboratorId) {
     Array<HourBankTransaction> transactions = Array.createAsEmpty();
     var overtime = Time.create(overtimeValue);
     var latetime = Time.create(latetimeValue);
 
     if (overtime.isZero().isFalse()) {
-      var creditTransaction = HourBankTransaction.create(overtime.value(), "CREDIT");
+      var dto = new HourBankTransactionDto()
+          .setTime(overtime.value())
+          .setDate(date)
+          .setOperation("CREDIT")
+          .setReason("OVERTIME");
+      var creditTransaction = HourBankTransaction.create(dto);
       transactions.add(creditTransaction);
     }
 
     if (latetime.isZero().isFalse()) {
-      var debitTransaction = HourBankTransaction.create(latetime.value(), "DEBIT");
+      var dto = new HourBankTransactionDto()
+          .setTime(overtime.value())
+          .setDate(date)
+          .setOperation("DEBIT")
+          .setReason("LATETIME");
+      var debitTransaction = HourBankTransaction.create(dto);
       transactions.add(debitTransaction);
     }
 
