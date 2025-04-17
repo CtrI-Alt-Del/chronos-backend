103ba8b5a6377e38d7ad39807f3e9f14a2ae95da
kaufon <kauanfvale@gmail.com>
2025-04-17 20:23:58 -0300

    🌐domain: add justification type repository interface

---
+++ src/main/java/br/com/chronos/core/solicitation/interfaces/repositories/JustificationTypeRepository.java
@@ -0,0 +1,18 @@
+
+package br.com.chronos.core.solicitation.interfaces.repositories;
+
+import java.util.Optional;
+
+import br.com.chronos.core.global.domain.records.Array;
+import br.com.chronos.core.global.domain.records.Id;
+import br.com.chronos.core.solicitation.domain.entities.JustificationType;
+
+public interface JustificationTypeRepository {
+
+  void add(JustificationType justificationType);
+  void remove(JustificationType justificationType);
+  Optional<JustificationType> findById(Id id);
+  void replace(JustificationType justificationType);
+  Array<JustificationType> findAll();
+
+}
