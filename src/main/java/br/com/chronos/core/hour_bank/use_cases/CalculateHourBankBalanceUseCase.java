f1ae91326eba45ff47c76053d043b26720c245ca
JohnPetros <joaopedro.nc@outlook.com>
2025-04-17 15:29:27 -0300

    ♻️ refactor: use hour bank balance record in CalculateHourBankBalanceUseCase

--- src/main/java/br/com/chronos/core/hour_bank/use_cases/CalculateHourBankBalanceUseCase.java
+++ src/main/java/br/com/chronos/core/hour_bank/use_cases/CalculateHourBankBalanceUseCase.java
@@ -1,10 +1,8 @@
 package br.com.chronos.core.hour_bank.use_cases;
 
-import java.time.LocalTime;
-
 import br.com.chronos.core.global.domain.records.Id;
-import br.com.chronos.core.global.domain.records.Time;
-import br.com.chronos.core.hour_bank.domain.records.HourBankTransaction;
+import br.com.chronos.core.hour_bank.domain.dtos.HourBankBalanceDto;
+import br.com.chronos.core.hour_bank.domain.records.HourBankBalance;
 import br.com.chronos.core.hour_bank.interfaces.HourBankTransactionsRepository;
 
 public class CalculateHourBankBalanceUseCase {
@@ -14,21 +12,9 @@ public class CalculateHourBankBalanceUseCase {
     this.repository = repository;
   }
 
-  public LocalTime execute(String collaboratorId) {
+  public HourBankBalanceDto execute(String collaboratorId) {
     var transactions = repository.findAllByCollaborator(Id.create(collaboratorId));
-    var balanceTime = Time.createAsZero();
-    var creditTime = Time.createAsZero();
-    var debitTime = Time.createAsZero();
-
-    for (HourBankTransaction transaction : transactions.list()) {
-      if (transaction.operation().isCreditOperation().isTrue()) {
-        debitTime = debitTime.plus(transaction.time());
-      } else {
-        debitTime = debitTime.plus(transaction.time());
-      }
-    }
-
-    balanceTime = balanceTime.plus(creditTime).minus(debitTime);
-    return balanceTime.value();
+    var balance = HourBankBalance.create(transactions);
+    return balance.getDto();
   }
 }
