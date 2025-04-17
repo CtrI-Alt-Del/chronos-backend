b67d547b37f14a6879c6d48a94ac77e62f8d27e1
JohnPetros <joaopedro.nc@outlook.com>
2025-04-17 15:31:57 -0300

    🐛 fix: response type in CalculateHourBankBalanceController

--- src/main/java/br/com/chronos/server/api/controllers/hour_bank/CalculateHourBankBalanceController.java
+++ src/main/java/br/com/chronos/server/api/controllers/hour_bank/CalculateHourBankBalanceController.java
@@ -1,13 +1,10 @@
 package br.com.chronos.server.api.controllers.hour_bank;
 
-import java.time.LocalTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
+import br.com.chronos.core.hour_bank.domain.dtos.HourBankBalanceDto;
 import br.com.chronos.core.hour_bank.interfaces.HourBankTransactionsRepository;
 import br.com.chronos.core.hour_bank.use_cases.CalculateHourBankBalanceUseCase;
 
@@ -16,17 +13,10 @@ public class CalculateHourBankBalanceController {
   @Autowired
   private HourBankTransactionsRepository hourBankTransactionsRepository;
 
-  @Data
-  @AllArgsConstructor
-  private static class Response {
-    private LocalTime balance;
-  }
-
   @GetMapping("/{collaboratorId}/balance")
-  public ResponseEntity<Response> handle(@PathVariable String collaboratorId) {
+  public ResponseEntity<HourBankBalanceDto> handle(@PathVariable String collaboratorId) {
     var useCase = new CalculateHourBankBalanceUseCase(hourBankTransactionsRepository);
     var hourBankBalance = useCase.execute(collaboratorId);
-    var response = new Response(hourBankBalance);
-    return ResponseEntity.ok(response);
+    return ResponseEntity.ok(hourBankBalance);
   }
 }
