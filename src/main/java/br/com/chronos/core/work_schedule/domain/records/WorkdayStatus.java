5526e493d141f92c7fddd1300f8fef32ab9beaab
JohnPetros <joaopedro.nc@outlook.com>
2025-04-16 21:10:08 -0300

    🌐 domain: make WorkdayStatus verify its own status

--- src/main/java/br/com/chronos/core/work_schedule/domain/records/WorkdayStatus.java
+++ src/main/java/br/com/chronos/core/work_schedule/domain/records/WorkdayStatus.java
@@ -1,6 +1,7 @@
 package br.com.chronos.core.work_schedule.domain.records;
 
 import br.com.chronos.core.global.domain.exceptions.ValidationException;
+import br.com.chronos.core.global.domain.records.Logical;
 
 public record WorkdayStatus(WorkdayStatusName name) {
   public enum WorkdayStatusName {
@@ -32,12 +33,36 @@ public record WorkdayStatus(WorkdayStatusName name) {
     return new WorkdayStatus(WorkdayStatusName.DAY_OFF);
   }
 
+  public static WorkdayStatus createAsAbsence() {
+    return new WorkdayStatus(WorkdayStatusName.ABSENCE);
+  }
+
   public static WorkdayStatus createAsHoliday() {
     return new WorkdayStatus(WorkdayStatusName.HOLIDAY);
   }
 
   public static WorkdayStatus createAsWorkLeave() {
     return new WorkdayStatus(WorkdayStatusName.WORK_LEAVE);
+  }
+
+  public Logical isNormalDay() {
+    return Logical.create(name == WorkdayStatusName.NORMAL_DAY);
+  }
+
+  public Logical isAbsence() {
+    return Logical.create(name == WorkdayStatusName.ABSENCE);
+  }
+
+  public Logical isDayOff() {
+    return Logical.create(name == WorkdayStatusName.DAY_OFF);
+  }
+
+  public Logical isHoliday() {
+    return Logical.create(name == WorkdayStatusName.HOLIDAY);
+  }
+
+  public Logical isWorkLeave() {
+    return Logical.create(name == WorkdayStatusName.WORK_LEAVE);
   }
 
   public String toString() {
