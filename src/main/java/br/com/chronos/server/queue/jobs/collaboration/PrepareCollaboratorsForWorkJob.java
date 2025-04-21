4fccee096854cf7bb6ccf613b46eb47e861fcd67
JohnPetros <joaopedro.nc@outlook.com>
2025-04-21 17:49:34 -0300

    🎞️ queue: adapt CloseWorkdayJob for america sao paulo and asia tokyo time zones

--- src/main/java/br/com/chronos/server/queue/jobs/collaboration/PrepareCollaboratorsForWorkJob.java
+++ src/main/java/br/com/chronos/server/queue/jobs/collaboration/PrepareCollaboratorsForWorkJob.java
@@ -1,6 +1,7 @@
 package br.com.chronos.server.queue.jobs.collaboration;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -18,17 +19,21 @@ public class PrepareCollaboratorsForWorkJob {
   @Autowired
   private CollaborationBroker collaborationBroker;
 
-  @Scheduled(cron = "0 5 0 * * ?", zone = "America/Sao_Paulo")
+  private static final String CRON_EXPRESSION = "0 5 0 * * ?";
+  private static final String AMERICA_SAO_PAULO_ZONE_ID = "America/Sao_Paulo";
+  private static final String ASIA_TOKYO_ZONE_ID = "Asia/Tokyo";
+
+  @Scheduled(cron = CRON_EXPRESSION, zone = AMERICA_SAO_PAULO_ZONE_ID)
   public void handleInAmericaSaoPaulo() {
-    executeUseCase();
+    executeUseCase(ZoneId.of(AMERICA_SAO_PAULO_ZONE_ID));
   }
 
-  @Scheduled(cron = "0 5 0 * * ?", zone = "Asia/Tokyo")
+  @Scheduled(cron = CRON_EXPRESSION, zone = ASIA_TOKYO_ZONE_ID)
   public void handleInAsiaTokyo() {
-    executeUseCase();
+    executeUseCase(ZoneId.of(ASIA_TOKYO_ZONE_ID));
   }
 
-  private void executeUseCase() {
+  private void executeUseCase(ZoneId zoneId) {
     var useCase = new PrepareCollaboratorsForWorkUseCase(collaboratorsRepository, collaborationBroker);
     useCase.execute(LocalDate.now());
   }
--- src/main/java/br/com/chronos/server/queue/jobs/work_schedule/CloseWorkdayJob.java
+++ src/main/java/br/com/chronos/server/queue/jobs/work_schedule/CloseWorkdayJob.java
@@ -1,6 +1,7 @@
 package br.com.chronos.server.queue.jobs.work_schedule;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -18,9 +19,22 @@ public class CloseWorkdayJob {
   @Autowired
   private WorkScheduleBroker workScheduleBroker;
 
-  @Scheduled(cron = "0 59 23 * * ?")
-  public void handle() {
+  private static final String CRON_EXPRESSION = "0 59 23 * * ?";
+  private static final String AMERICA_SAO_PAULO_ZONE_ID = "America/Sao_Paulo";
+  private static final String ASIA_TOKYO_ZONE_ID = "Asia/Tokyo";
+
+  @Scheduled(cron = CRON_EXPRESSION, zone = AMERICA_SAO_PAULO_ZONE_ID)
+  public void handleInAmericaSaoPaulo() {
+    executeUseCase(ZoneId.of(AMERICA_SAO_PAULO_ZONE_ID));
+  }
+
+  @Scheduled(cron = CRON_EXPRESSION, zone = ASIA_TOKYO_ZONE_ID)
+  public void handleInAsiaTokyo() {
+    executeUseCase(ZoneId.of(ASIA_TOKYO_ZONE_ID));
+  }
+
+  private void executeUseCase(ZoneId zoneId) {
     var useCase = new CloseWorkdayUseCase(workdayLogsRepository, workScheduleBroker);
-    useCase.execute(LocalDate.now());
+    useCase.execute(LocalDate.now(zoneId));
   }
 }
