e4e0db0536796663617107cfcd4f53819df9750e
kaufon <kauanfvale@gmail.com>
2025-04-22 20:09:58 -0300

    💾database: add attachemnt mapper

--- src/main/java/br/com/chronos/server/database/jpa/solicitation/models/JustificationTypeModel.java
+++ src/main/java/br/com/chronos/server/database/jpa/solicitation/models/JustificationTypeModel.java
@@ -1,10 +1,13 @@
 package br.com.chronos.server.database.jpa.solicitation.models;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,4 +30,8 @@ public class JustificationTypeModel {
 
   @Column
   private Boolean shouldHaveAttachment;
+
+  @OneToMany(mappedBy = "justificationType")
+  @Builder.Default
+  private List<JustificationModel> justification = new ArrayList<>();
 }
