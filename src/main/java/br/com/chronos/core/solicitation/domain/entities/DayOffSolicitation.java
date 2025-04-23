2fafbaa23ac6ead210459a146a54bb948a8561c1
kaufon <kauanfvale@gmail.com>
2025-04-22 23:21:54 -0300

    🌐domain: add day off solicitation entity

---
+++ src/main/java/br/com/chronos/core/solicitation/domain/entities/DayOffSolicitation.java
@@ -0,0 +1,37 @@
+package br.com.chronos.core.solicitation.domain.entities;
+
+import br.com.chronos.core.global.domain.records.Date;
+import br.com.chronos.core.solicitation.domain.abstracts.Solicitation;
+import br.com.chronos.core.solicitation.domain.dtos.DayOffSolicitationDto;
+import br.com.chronos.core.solicitation.domain.records.SolicitationType;
+
+public class DayOffSolicitation extends Solicitation {
+  private Justification justification;
+  private Date dayOff;
+  public DayOffSolicitation(DayOffSolicitationDto dto){
+    super(dto);
+    this.type = SolicitationType.createAsDayOff();
+    dayOff = Date.create(dto.dayOff);
+  }
+  public DayOffSolicitationDto getDto(){
+    DayOffSolicitationDto dto = new DayOffSolicitationDto();
+    dto.setJustification(getJustification().getDto());
+    dto.setId(getId().toString());
+    dto.setDescription(getDescription() != null ? getDescription().value() : null);
+    dto.setDate(getDate().value());
+    dto.setStatus(getStatus().value().toString());
+    dto.setFeedbackMessage(getFeedbackMessage() != null ? getFeedbackMessage().value() : null);
+    dto.setSenderResponsible(getSenderResponsible().getDto());
+    dto.setReplierResponsible(getReplierResponsible() != null ? getReplierResponsible().getDto() : null);
+    dto.setDayOff(getDayOff().value());
+    dto.setType(getType().value().toString());
+    return dto;
+  }
+  public Justification getJustification() {
+    return justification;
+  }
+  public Date getDayOff() {
+    return dayOff;
+  }
+
+}
