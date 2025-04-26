61d08bfc57b86660c536216142e645416c85491c
kaufon <kauanfvale@gmail.com>
2025-04-26 12:57:33 -0300

    🌐domain: add new method to solicitation repository

--- src/main/java/br/com/chronos/core/solicitation/interfaces/repositories/SolicitationsRepository.java
+++ src/main/java/br/com/chronos/core/solicitation/interfaces/repositories/SolicitationsRepository.java
@@ -6,11 +6,13 @@ import br.com.chronos.core.global.domain.records.Array;
 import br.com.chronos.core.global.domain.records.CollaborationSector;
 import br.com.chronos.core.global.domain.records.Id;
 import br.com.chronos.core.solicitation.domain.abstracts.Solicitation;
+import br.com.chronos.core.solicitation.domain.entities.ExcuseAbsenceSolicitation;
 import br.com.chronos.core.solicitation.domain.entities.PaidOvertimeSolicitation;
 import br.com.chronos.core.solicitation.domain.records.SolicitationType;
 
 public interface SolicitationsRepository {
   void add(PaidOvertimeSolicitation solicitation);
+  void add(ExcuseAbsenceSolicitation solicitation);
 
   Array<Solicitation> findAllByCollaboratorId(Id collaboratorId);
 
