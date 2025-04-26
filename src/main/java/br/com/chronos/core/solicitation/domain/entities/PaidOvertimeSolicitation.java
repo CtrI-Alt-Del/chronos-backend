3e54d9f5ebe5338a874419d65eb13575a2b09ecd
JohnPetros <joaopedro.nc@outlook.com>
2025-04-26 10:40:31 -0300

    🌐 domain: remove overtime constant from PaidOvertimeSolicitation

--- src/main/java/br/com/chronos/core/solicitation/domain/entities/PaidOvertimeSolicitation.java
+++ src/main/java/br/com/chronos/core/solicitation/domain/entities/PaidOvertimeSolicitation.java
@@ -1,12 +1,9 @@
 package br.com.chronos.core.solicitation.domain.entities;
 
-import br.com.chronos.core.global.domain.records.Time;
 import br.com.chronos.core.solicitation.domain.abstracts.Solicitation;
 import br.com.chronos.core.solicitation.domain.dtos.PaidOvertimeSolicitationDto;
 
 public final class PaidOvertimeSolicitation extends Solicitation {
-  static final Time OVERTIME = Time.create(2, 0);
-
   public PaidOvertimeSolicitation(PaidOvertimeSolicitationDto dto) {
     super(dto);
   }
