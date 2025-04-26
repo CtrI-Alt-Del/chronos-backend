518bb24b4df922c74af33fd73f988aab74017d57
JohnPetros <joaopedro.nc@outlook.com>
2025-04-26 10:37:32 -0300

    📶 rest: add EnsureSufficientHourBankCreditForDayOffSolicitationAdvice

---
+++ src/main/java/br/com/chronos/server/api/advices/collaboration/AttachCollaboratorWorkloadAdvice.java
@@ -0,0 +1,40 @@
+package br.com.chronos.server.api.advices.collaboration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import br.com.chronos.core.collaboration.interfaces.repositories.CollaboratorsRepository;
+import br.com.chronos.core.collaboration.use_cases.GetCollaboratorUseCase;
+import br.com.chronos.core.global.interfaces.providers.AuthenticationProvider;
+import br.com.chronos.server.api.advices.Advice;
+import br.com.chronos.server.api.controllers.solicitation.solicitations.CreateDayOffSolicitationController;
+import br.com.chronos.server.api.controllers.solicitation.solicitations.CreatePaidOvertimeSolitationController;
+
+@ControllerAdvice
+@Order(1)
+public class AttachCollaboratorWorkloadAdvice extends Advice {
+  @Autowired
+  private AuthenticationProvider authenticationProvider;
+
+  @Autowired
+  private CollaboratorsRepository collaboratorsRepository;
+
+  public AttachCollaboratorWorkloadAdvice() {
+    super(CreatePaidOvertimeSolitationController.class);
+  }
+
+  @Override
+  protected Object handle(Object body) {
+    if (body instanceof CreateDayOffSolicitationController.Request) {
+      var request = (CreateDayOffSolicitationController.Request) body;
+      var account = authenticationProvider.getAccount();
+      var useCase = new GetCollaboratorUseCase(collaboratorsRepository);
+      var collaborator = useCase.execute(account.getCollaboratorId().toString());
+      request.setWorkload(collaborator.workload);
+      return request;
+    }
+
+    return body;
+  }
+}
