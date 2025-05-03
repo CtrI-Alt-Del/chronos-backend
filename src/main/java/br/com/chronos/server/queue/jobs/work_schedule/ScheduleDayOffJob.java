5a0036b51630f32d770a9fb7d1dbb068ac3031ed
JohnPetros <joaopedro.nc@outlook.com>
2025-05-03 20:53:49 -0300

    🎞️ queue: add ScheduleDayOffJob

---
+++ src/main/java/br/com/chronos/server/queue/jobs/work_schedule/ScheduleDayOffJob.java
@@ -0,0 +1,21 @@
+package br.com.chronos.server.queue.jobs.work_schedule;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import br.com.chronos.core.solicitation.domain.events.DayOffSolicitationApprovedEvent;
+import br.com.chronos.core.work_schedule.interfaces.repositories.DayOffSchedulesRepository;
+import br.com.chronos.core.work_schedule.use_cases.ScheduleDayOffUseCase;
+
+@Component
+public class ScheduleDayOffJob {
+  public static final String KEY = "work.schedule/schedule.day.off.job";
+
+  @Autowired
+  private DayOffSchedulesRepository dayOffSchedulesRepository;
+
+  public void handle(DayOffSolicitationApprovedEvent.Payload payload) {
+    var useCase = new ScheduleDayOffUseCase(dayOffSchedulesRepository);
+    useCase.execute(payload.dayOff(), payload.collaboratorId());
+  }
+}
