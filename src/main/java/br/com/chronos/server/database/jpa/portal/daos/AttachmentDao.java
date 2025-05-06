1a9ffaa82e353329d9ee9a0123145f7ee7183639
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-05-06 20:25:18 -0300

    💾 databse:  add AttachmentDao

---
+++ src/main/java/br/com/chronos/server/database/jpa/portal/daos/AttachmentDao.java
@@ -0,0 +1,9 @@
+package br.com.chronos.server.database.jpa.portal.daos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.chronos.server.database.jpa.portal.models.AttachmentModel;
+
+public interface AttachmentDao extends JpaRepository<AttachmentModel, String> {
+
+}
