e3076317c225db7989f6364c96a77fd580e344ee
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-05-12 06:24:54 -0300

    🎞️ queue: add NotificationExchange

--- src/main/java/br/com/chronos/server/queue/rabbitmq/brokers/RabbitMqAuthBroker.java
+++ src/main/java/br/com/chronos/server/queue/rabbitmq/brokers/RabbitMqAuthBroker.java
@@ -4,8 +4,10 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
 import br.com.chronos.core.auth.domain.events.AccountUpdatedEvent;
+import br.com.chronos.core.auth.domain.events.AuthenticationRequestedEvent;
 import br.com.chronos.core.auth.interfaces.AuthBroker;
 import br.com.chronos.server.queue.rabbitmq.exchanges.CollaborationExchange;
+import br.com.chronos.server.queue.rabbitmq.exchanges.NotificationExchange;
 
 @Component
 public class RabbitMqAuthBroker implements AuthBroker {
@@ -20,6 +22,14 @@ public class RabbitMqAuthBroker implements AuthBroker {
     rabbit.convertAndSend(
         CollaborationExchange.NAME,
         AccountUpdatedEvent.NAME,
+        event.getPayload());
+  }
+
+  @Override
+  public void publish(AuthenticationRequestedEvent event) {
+    rabbit.convertAndSend(
+        NotificationExchange.NAME,
+        AuthenticationRequestedEvent.NAME,
         event.getPayload());
   }
 }
