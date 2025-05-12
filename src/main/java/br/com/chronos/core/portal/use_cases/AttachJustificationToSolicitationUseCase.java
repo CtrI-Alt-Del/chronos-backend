cb9b37784aa7a57b484e85fdf9470a25ff2a93ac
kaufon <kauanfvale@gmail.com>
2025-05-12 09:47:57 -0300

    ♻️refactor: completly remove all aspects

--- src/main/java/br/com/chronos/core/portal/use_cases/AttachJustificationToSolicitationUseCase.java
+++ src/main/java/br/com/chronos/core/portal/use_cases/AttachJustificationToSolicitationUseCase.java
@@ -1,11 +1,15 @@
 package br.com.chronos.core.portal.use_cases;
 
+import br.com.chronos.core.global.domain.dtos.AttachmentDto;
 import br.com.chronos.core.global.domain.exceptions.NotFoundException;
+import br.com.chronos.core.global.domain.records.Attachment;
 import br.com.chronos.core.global.domain.records.Id;
+import br.com.chronos.core.global.interfaces.providers.StorageProvider;
 import br.com.chronos.core.portal.domain.dtos.ExcusedAbsenceSolicitationDto;
 import br.com.chronos.core.portal.domain.dtos.JustificationDto;
 import br.com.chronos.core.portal.domain.entities.ExcusedAbsenceSolicitation;
 import br.com.chronos.core.portal.domain.entities.Justification;
+import br.com.chronos.core.portal.interfaces.repositories.AttachmentRepository;
 import br.com.chronos.core.portal.interfaces.repositories.JustificationRepository;
 import br.com.chronos.core.portal.interfaces.repositories.SolicitationsRepository;
 
@@ -13,24 +17,32 @@ public class AttachJustificationToSolicitationUseCase {
   private final SolicitationsRepository solicitationsRepository;
   private final JustificationRepository justificationRepository;
 
-  public AttachJustificationToSolicitationUseCase(SolicitationsRepository solicitationsRepository,
-      JustificationRepository justificationRepository) {
+  public AttachJustificationToSolicitationUseCase(
+      SolicitationsRepository solicitationsRepository,
+      JustificationRepository justificationRepository){
     this.justificationRepository = justificationRepository;
     this.solicitationsRepository = solicitationsRepository;
   }
 
   public ExcusedAbsenceSolicitationDto execute(String solicitationId,
-      JustificationDto justificationDto) {
-    var solicitation = findSolicitation(Id.create(solicitationId));
+      JustificationDto justificationDto,AttachmentDto attachmentDto) {
+
+    if(attachmentDto != null ){
+      justificationDto.setAttachment(attachmentDto);
+    }
+
     var justification = new Justification(justificationDto);
+    var solicitation = findSolicitation(Id.create(solicitationId));
+
     justificationRepository.add(justification);
     solicitationsRepository.addJustificationToSolicitation(solicitation,
         justification);
+
     return findSolicitation(Id.create(solicitationId)).getDto();
   }
 
   private ExcusedAbsenceSolicitation findSolicitation(Id solicitationId) {
-    var solicitation = solicitationsRepository.findExcusedAbsenceSolicitationById(solicitationId);
+    var solicitation = solicitationsRepository.findById(solicitationId);
     if (solicitation.isEmpty()) {
       throw new NotFoundException("Solicitacao nao encontrada");
     }
