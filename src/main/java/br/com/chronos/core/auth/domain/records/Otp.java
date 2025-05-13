7ec697477f6928cc623988c80f2154c31cc5212c
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-05-12 23:15:23 -0300

    🌐 domain: make Otp record validation otp codes

--- src/main/java/br/com/chronos/core/auth/domain/records/Otp.java
+++ src/main/java/br/com/chronos/core/auth/domain/records/Otp.java
@@ -2,17 +2,24 @@ package br.com.chronos.core.auth.domain.records;
 
 import java.util.concurrent.ThreadLocalRandom;
 
+import br.com.chronos.core.global.domain.exceptions.ValidationException;
+import br.com.chronos.core.global.domain.records.PlusIntegerNumber;
 import br.com.chronos.core.global.domain.records.Text;
 
 public record Otp(Text code) {
   public static final int MINUTES_TO_EXPIRE = 60;
 
-  public static Otp create(String code) {
-    return new Otp(Text.create(code, "otp code"));
+  public static Otp create(String codeValue) {
+    var code = Text.create(codeValue, "código de autenticação");
+
+    if (code.charactersCount().isEqual(PlusIntegerNumber.create(6)).isFalse()) {
+      throw new ValidationException("código de autenticação", "deve conter 6 dígitos");
+    }
+    return new Otp(code);
   }
 
   public static Otp create() {
-    var code = String.format("%04d", ThreadLocalRandom.current().nextInt(0, 10000));
+    var code = String.format("%06d", ThreadLocalRandom.current().nextInt(0, 1000000));
     return new Otp(Text.create(code, "otp code"));
   }
 }
