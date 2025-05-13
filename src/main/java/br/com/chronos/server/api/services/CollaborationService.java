b4fbdec9e8c5d7d84fb9c6d2cfed36c0233ab6af
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-05-13 12:20:44 -0300

    🚧 wip: CollaborationService

---
+++ src/main/java/br/com/chronos/server/api/services/CollaborationService.java
@@ -0,0 +1,9 @@
+package br.com.chronos.server.api.services;
+
+import java.util.List;
+
+public class CollaborationService {
+  public List<String> getManagersEmails(String collaboratorSector) {
+    return null;
+  }
+}
