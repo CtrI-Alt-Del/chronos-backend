972dc681d8045f2319a6c8c347367b6b1163d0b0
eduardokuwaharajr57@gmail.com <eduardokuwaharajr57@gmail.com>
2025-05-15 20:04:45 -0300

    ✨ use case: Implemeting a new event and method

--- src/main/java/br/com/chronos/core/portal/use_cases/ApproveVacationSolicitationUseCase.java
+++ src/main/java/br/com/chronos/core/portal/use_cases/ApproveVacationSolicitationUseCase.java
@@ -2,6 +2,10 @@ package br.com.chronos.core.portal.use_cases;
 
 import br.com.chronos.core.global.domain.dtos.ResponsibleAggregateDto;
 import br.com.chronos.core.global.domain.records.Id;
+import br.com.chronos.core.portal.domain.entities.VacationSolicitation;
+import br.com.chronos.core.portal.domain.events.VacationSolicitationApprovedEvent;
+import br.com.chronos.core.portal.domain.exceptions.SolicitationNotFoundException;
+import br.com.chronos.core.portal.domain.records.SolicitationType;
 import br.com.chronos.core.portal.interfaces.PortalBroker;
 import br.com.chronos.core.portal.interfaces.repositories.SolicitationsRepository;
 
@@ -19,7 +23,19 @@ public class ApproveVacationSolicitationUseCase extends ApproveSolicitationUseCa
         String feedbackMessage) {
     var solicitation = findSolicitation(Id.create(solicitationId));
 
-        repository.replace(solicitation);
-    
-        }} 
+    repository.replace(solicitation);
+
+    var event = new VacationSolicitationApprovedEvent(solicitation);
+    portalBroker.publish(event);
+  }
+
+  private VacationSolicitation findSolicitation(Id solicitationId) {
+    var solicitation = repository.findSolicitationByIdAndSolicitationType(solicitationId,
+        SolicitationType.createAsVacation());
+    if (solicitation.isEmpty()) {
+      throw new SolicitationNotFoundException();
+    }
+    return (VacationSolicitation) solicitation.get();
+} 
+}
 
