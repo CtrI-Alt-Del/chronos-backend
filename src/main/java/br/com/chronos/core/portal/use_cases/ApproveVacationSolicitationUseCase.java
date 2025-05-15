2284ae2c966cd864de9f34bb11352b5de9532dd2
eduardokuwaharajr57@gmail.com <eduardokuwaharajr57@gmail.com>
2025-05-15 19:07:41 -0300

    🚧 wip: creating use case approve vacation solicitation

--- src/main/java/br/com/chronos/core/portal/use_cases/ApproveVacationSolicitationUseCase.java
+++ src/main/java/br/com/chronos/core/portal/use_cases/ApproveVacationSolicitationUseCase.java
@@ -1,5 +1,25 @@
 package br.com.chronos.core.portal.use_cases;
 
-public class ApproveVacationSolicitationUseCase {
+import br.com.chronos.core.global.domain.dtos.ResponsibleAggregateDto;
+import br.com.chronos.core.global.domain.records.Id;
+import br.com.chronos.core.portal.interfaces.PortalBroker;
+import br.com.chronos.core.portal.interfaces.repositories.SolicitationsRepository;
+
+public class ApproveVacationSolicitationUseCase extends ApproveSolicitationUseCase {
+  public ApproveVacationSolicitationUseCase(
+      SolicitationsRepository repository,
+      PortalBroker portalBroker) {
+    super(repository, portalBroker);
+  }
+
+  public void execute(
+        String solicitationId,
+        ResponsibleAggregateDto replierResponsible,
+        byte collaboratorWorkload,
+        String feedbackMessage) {
+    var solicitation = findSolicitation(Id.create(solicitationId));
+
+        repository.replace(solicitation);
     
-}
+        }} 
+
