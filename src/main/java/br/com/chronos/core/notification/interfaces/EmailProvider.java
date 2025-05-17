c0e4794965f3d7f646363621b913c33c3084bb75
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-05-17 11:40:34 -0300

    🧰 provision: implement all methods in EmailProvider for solicitations email sending using Spring Mail Sender

--- src/main/java/br/com/chronos/core/notification/interfaces/EmailProvider.java
+++ src/main/java/br/com/chronos/core/notification/interfaces/EmailProvider.java
@@ -6,9 +6,9 @@ import br.com.chronos.core.global.domain.records.Text;
 public interface EmailProvider {
   void sendAuthenticationEmail(Email recipientEmail, Text otpCode);
 
-  void sendSolicitationCreatedEmail(Email managerEmail, Text solicitationType);
+  void sendSolicitationCreationEmail(Email managerEmail, Text employeeName, Text solicitationType);
 
-  void sendSolicitationApprovedEmail(Email managerEmail, Text solicitationType);
+  void sendSolicitationApprovalEmail(Email employeeEmail, Text employeeName, Text solicitationType);
 
-  void sendSolicitationDeniedEmail(Email managerEmail, Text solicitationType);
+  void sendSolicitationDenialEmail(Email employeeEmail, Text employeeName, Text solicitationType);
 }
