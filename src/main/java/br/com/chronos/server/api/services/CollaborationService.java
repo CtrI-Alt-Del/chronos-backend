49f1e322ecba46d1fd2f28db1e8f97d93f5b1749
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-05-17 11:46:33 -0300

    📶 rest: add CollaborationService

--- src/main/java/br/com/chronos/server/api/services/CollaborationService.java
+++ src/main/java/br/com/chronos/server/api/services/CollaborationService.java
@@ -1,7 +1,10 @@
 package br.com.chronos.server.api.services;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Service;
+
+import br.com.chronos.core.collaboration.domain.dtos.CollaboratorDto;
 
 @Service
 public class CollaborationService extends RestService {
@@ -10,6 +13,23 @@ public class CollaborationService extends RestService {
   }
 
   public List<String> getManagersEmails(String collaboratorationSector) {
-    return get("collaborators/managers/emails?sector=" + collaboratorationSector);
+    var response = get("collaborators/managers/emails?sector=" + collaboratorationSector);
+    if (response.get("item") instanceof List) {
+      return (List<String>) response.get("item");
+    }
+    return new ArrayList<String>();
+  }
+
+  public CollaboratorDto getCollaborator(String collaboratorId) {
+    var response = get("collaborators/" + collaboratorId);
+    return new CollaboratorDto()
+        .setId(response.get("id").toString())
+        .setName(response.get("name").toString())
+        .setEmail(response.get("email").toString())
+        .setCpf(response.get("cpf").toString())
+        .setRole(response.get("role").toString())
+        .setSector(response.get("sector").toString())
+        .setActive(Boolean.parseBoolean(response.get("isActive").toString()))
+        .setWorkload(Byte.parseByte(response.get("workload").toString()));
   }
 }
