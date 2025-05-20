24630e6dda14f03842b1475cfe5adc73d237dd57
eduardokuwaharajr57@gmail.com <eduardokuwaharajr57@gmail.com>
2025-05-19 22:10:03 -0300

    ♻ refactor: Adding new arguments for payload

--- src/main/java/br/com/chronos/core/portal/domain/events/VacationSolicitationApprovedEvent.java
+++ src/main/java/br/com/chronos/core/portal/domain/events/VacationSolicitationApprovedEvent.java
@@ -6,15 +6,14 @@ import br.com.chronos.core.portal.domain.entities.VacationSolicitation;
 public class VacationSolicitationApprovedEvent extends Event<VacationSolicitationApprovedEvent.Payload> {
     public static final String NAME = "portal/vacation.solicitation.approved";
 
-    public static record Payload(String collaboratorId, String vacation) {
+    public static record Payload(String collaboratorId, String startDate, String endDate) {
     }
 
     public VacationSolicitationApprovedEvent(VacationSolicitation solicitation) {
         super(new Payload(
             solicitation.getSenderResponsible().getId().toString(),
-            solicitation.getVacationDays().firstItem().value() + " - " +
-            solicitation.getVacationDays().lastItem().value() + " (" +
-            solicitation.getVacationDays().size().value()
+            solicitation.getVacationDays().firstItem().value().toString(),
+            solicitation.getVacationDays().lastItem().value().toString()
         ));
     }
     
